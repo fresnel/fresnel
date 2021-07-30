1fb8d636a95ebe29e2260369d0757a77f5d5a2ae
Rob Rix <rob.rix@me.com>
2021-07-30 19:43:00 -0400

    Define a Profunctor instance for Recall.

--- src/Fresnel/Profunctor/Recall.hs
+++ src/Fresnel/Profunctor/Recall.hs
@@ -3,7 +3,13 @@ module Fresnel.Profunctor.Recall
   Recall(..)
 ) where
 
+import Data.Profunctor
+
 -- * Recall profunctor
 
 newtype Recall e a b = Recall { runRecall :: e -> b }
   deriving (Applicative, Functor, Monad)
+
+instance Profunctor (Recall e) where
+  dimap _ g = Recall . fmap g . runRecall
+  rmap = fmap
