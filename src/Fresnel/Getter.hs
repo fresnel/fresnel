67933f7cea2a686a318c9503c0b0ba0ff6d2ef45
Rob Rix <rob.rix@me.com>
2021-07-30 19:18:08 -0400

    Define a type of Getters.

--- src/Fresnel/Getter.hs
+++ src/Fresnel/Getter.hs
@@ -1,2 +1,9 @@
 module Fresnel.Getter
-() where
+( -- * Getters
+  Getter
+) where
+
+import Data.Profunctor
+import Fresnel.Optic
+
+type Getter s a = forall p . (Strong p, Cochoice p) => Optic' p s a
