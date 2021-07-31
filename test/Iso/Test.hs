17c6570d31ba0f5cc425ad74df7cb55fcc758cad
Rob Rix <rob.rix@me.com>
2021-07-31 14:08:10 -0400

    Test the review elimination law.

--- test/Iso/Test.hs
+++ test/Iso/Test.hs
@@ -5,9 +5,12 @@ module Iso.Test
 
 import Fresnel.Getter
 import Fresnel.Iso
+import Fresnel.Review
 import Test.QuickCheck
 
 prop_view_elimination f g x = view (iso (applyFun f) (applyFun g)) x === applyFun f x
+
+prop_review_elimination f g x = review (iso (applyFun f) (applyFun g)) x === applyFun g x
 
 
 pure []
