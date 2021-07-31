4549061f1d50c9129615c43c7efb31460c9e20be
Rob Rix <rob.rix@me.com>
2021-07-30 22:39:47 -0400

    Define a Profunctor instance for Coexp.

--- src/Fresnel/Profunctor/Coexp.hs
+++ src/Fresnel/Profunctor/Coexp.hs
@@ -2,5 +2,10 @@ module Fresnel.Profunctor.Coexp
 ( Coexp(..)
 ) where
 
+import Data.Profunctor
+
 data Coexp r e b a = Coexp (e -> a) (b -> r)
   deriving (Functor)
+
+instance Profunctor (Coexp r e) where
+  dimap f g (Coexp bt sa) = Coexp (g . bt) (sa . f)
