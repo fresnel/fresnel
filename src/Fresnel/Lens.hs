fdccd0cb81d9c1a3ec829370e240947d5edde6c8
Rob Rix <rob.rix@me.com>
2021-07-30 21:09:43 -0400

    Define a type of lenses.

--- src/Fresnel/Lens.hs
+++ src/Fresnel/Lens.hs
@@ -1,2 +1,11 @@
 module Fresnel.Lens
-() where
+( -- * Lenses
+  Lens
+) where
+
+import Data.Profunctor
+import Fresnel.Optic
+
+-- Lenses
+
+type Lens s t a b = forall p . Strong p => Optic p s t a b
