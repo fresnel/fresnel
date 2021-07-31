4408491525dba1b8463fd3151bf222d4852c79c7
Rob Rix <rob.rix@me.com>
2021-07-31 08:52:28 -0400

    Define a constructor for Traversal.

--- src/Fresnel/Traversal.hs
+++ src/Fresnel/Traversal.hs
@@ -2,6 +2,8 @@ module Fresnel.Traversal
 ( -- * Traversals
   Traversal
 , Traversal'
+  -- * Construction
+, traversed
 ) where
 
 import Data.Profunctor.Traversing
@@ -12,3 +14,9 @@ import Fresnel.Optic
 type Traversal s t a b = forall p . Traversing p => Optic p s t a b
 
 type Traversal' s a = Traversal s s a a
+
+
+-- Construction
+
+traversed :: Traversable t => Traversal (t a) (t b) a b
+traversed = wander traverse
