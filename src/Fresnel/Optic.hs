9e706a830a459e2190638e534cf829340856027a
Rob Rix <rob.rix@me.com>
2021-07-31 07:29:36 -0400

    Define Optic in terms of Optical.

--- src/Fresnel/Optic.hs
+++ src/Fresnel/Optic.hs
@@ -14,6 +14,6 @@ type Optical p q s t a b = (a `p` b) -> (s `q` t)
 type Optical' p q s a = Optical p q s s a a
 
 
-type Optic p s t a b = (a `p` b) -> (s `p` t)
+type Optic p s t a b = Optical p p s t a b
 
 type Optic' p s a = Optic p s s a a
