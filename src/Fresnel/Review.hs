f32dd13709baea37cb536600c38e5e193606fa26
Rob Rix <rob.rix@me.com>
2021-07-30 20:31:28 -0400

    Define a Review type.

--- src/Fresnel/Review.hs
+++ src/Fresnel/Review.hs
@@ -1,2 +1,12 @@
 module Fresnel.Review
-() where
+( -- * Reviews
+  Review
+) where
+
+import Data.Bifunctor
+import Data.Profunctor
+import Fresnel.Optic
+
+-- Reviews
+
+type Review t b = forall p . (Bifunctor p, Choice p, Costrong p) => Optic' p t b
