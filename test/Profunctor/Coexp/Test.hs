af26b45f040cbfb09a19d096819030d230253d06
Rob Rix <rob.rix@me.com>
2021-07-31 11:04:56 -0400

    Test the left identity law for Coexp.

--- test/Profunctor/Coexp/Test.hs
+++ test/Profunctor/Coexp/Test.hs
@@ -10,6 +10,9 @@ prop_semigroup_assoc :: (Eq b, Eq a, Show b, Show a) => ArbCoexp b a a b -> ArbC
 prop_semigroup_assoc a b c x y =
   appCoexp (coexp a <> (coexp b <> coexp c)) x y === appCoexp ((coexp a <> coexp b) <> coexp c) x y
 
+prop_monoid_left_identity :: (Eq b, Show b) => ArbCoexp b a a b -> b -> Property
+prop_monoid_left_identity a x = recall (mempty <> coexp a) x === recall (coexp a) x
+
 
 data ArbCoexp e r a b = ArbCoexp (Fun e b) (Fun a r)
   deriving (Show)
@@ -18,7 +21,7 @@ coexp :: ArbCoexp e r a b -> Coexp e r a b
 coexp (ArbCoexp eb ar) = Coexp (applyFun eb) (applyFun ar)
 
 appCoexp :: Coexp e r a b -> e -> a -> (b, r)
-appCoexp (Coexp eb ar) e a = (eb e, ar a)
+appCoexp c e a = (recall c e, forget c a)
 
 
 instance (Function e, Function a, CoArbitrary e, CoArbitrary a, Arbitrary b, Arbitrary r) => Arbitrary (ArbCoexp e r a b) where
