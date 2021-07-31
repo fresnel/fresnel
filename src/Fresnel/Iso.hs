16ba1a6d823838d071ddf72433ff603b9c54df69
Rob Rix <rob.rix@me.com>
2021-07-30 21:47:12 -0400

    Define an Iso type.

--- src/Fresnel/Iso.hs
+++ src/Fresnel/Iso.hs
@@ -1,2 +1,11 @@
 module Fresnel.Iso
-() where
+( -- * Isos
+  Iso
+) where
+
+import Data.Profunctor
+import Fresnel.Optic
+
+-- Isos
+
+type Iso s t a b = forall p . Profunctor p => Optic p s t a b
