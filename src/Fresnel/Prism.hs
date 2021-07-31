f6b866f05c4f5a97028d2f4ab0eacd8de144ecb7
Rob Rix <rob.rix@me.com>
2021-07-30 21:26:44 -0400

    Define a constructor for prisms.

--- src/Fresnel/Prism.hs
+++ src/Fresnel/Prism.hs
@@ -2,6 +2,8 @@ module Fresnel.Prism
 ( -- * Prisms
   Prism
 , Prism'
+  -- * Construction
+, prism
 ) where
 
 import Data.Profunctor
@@ -12,3 +14,9 @@ import Fresnel.Optic
 type Prism s t a b = forall p . Choice p => Optic p s t a b
 
 type Prism' s a = Prism s s a a
+
+
+-- Construction
+
+prism :: (b -> t) -> (s -> Either t a) -> Prism s t a b
+prism inj prj = dimap prj (either id inj) . right'
