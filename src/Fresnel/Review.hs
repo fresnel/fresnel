b4b001049a3c66327ad3510bb5289d467267a9fe
Rob Rix <rob.rix@me.com>
2021-07-30 20:37:04 -0400

    Define a reviews eliminator for Review.

--- src/Fresnel/Review.hs
+++ src/Fresnel/Review.hs
@@ -3,12 +3,15 @@ module Fresnel.Review
   Review
   -- * Construction
 , unto
+  -- * Elimination
+, reviews
 ) where
 
 import Data.Bifunctor
 import Data.Profunctor
 import Data.Void
 import Fresnel.Optic
+import Fresnel.Profunctor.Recall
 
 -- Reviews
 
@@ -19,3 +22,9 @@ type Review t b = forall p . (Bifunctor p, Profunctor p) => Optic' p t b
 
 unto :: (b -> t) -> Review t b
 unto f = first absurd . lmap absurd . rmap f
+
+
+-- Elimination
+
+reviews :: Review t b -> (e -> b) -> (e -> t)
+reviews b = runRecall . b . Recall
