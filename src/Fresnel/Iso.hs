4c7186d86b67ea8353464960f6ac61f0f8b508da
Rob Rix <rob.rix@me.com>
2021-07-30 21:47:41 -0400

    Define a monomorphic-update Iso type.

--- src/Fresnel/Iso.hs
+++ src/Fresnel/Iso.hs
@@ -1,6 +1,7 @@
 module Fresnel.Iso
 ( -- * Isos
   Iso
+, Iso'
 ) where
 
 import Data.Profunctor
@@ -9,3 +10,5 @@ import Fresnel.Optic
 -- Isos
 
 type Iso s t a b = forall p . Profunctor p => Optic p s t a b
+
+type Iso' s a = Iso s s a a
