86df9820b85f1f7b7288060db5c145a3f0561460
Rob Rix <rob.rix@me.com>
2021-07-31 14:07:29 -0400

    Test the view elimination law.

--- test/Iso/Test.hs
+++ test/Iso/Test.hs
@@ -3,7 +3,11 @@ module Iso.Test
 ( test
 ) where
 
+import Fresnel.Getter
+import Fresnel.Iso
 import Test.QuickCheck
+
+prop_view_elimination f g x = view (iso (applyFun f) (applyFun g)) x === applyFun f x
 
 
 pure []
