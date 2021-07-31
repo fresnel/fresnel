43b902f14420df338be70daf9056873c9c195af0
Rob Rix <rob.rix@me.com>
2021-07-30 20:35:42 -0400

    Define an unto constructor for Review.

--- src/Fresnel/Review.hs
+++ src/Fresnel/Review.hs
@@ -1,12 +1,21 @@
 module Fresnel.Review
 ( -- * Reviews
   Review
+  -- * Construction
+, unto
 ) where
 
 import Data.Bifunctor
 import Data.Profunctor
+import Data.Void
 import Fresnel.Optic
 
 -- Reviews
 
 type Review t b = forall p . (Bifunctor p, Profunctor p) => Optic' p t b
+
+
+-- Construction
+
+unto :: (b -> t) -> Review t b
+unto f = first absurd . lmap absurd . rmap f
