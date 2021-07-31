7e692b83912c1c3eba68a3acd9a9e83400166dcb
Rob Rix <rob.rix@me.com>
2021-07-30 22:46:24 -0400

    :memo: Coexp.

--- src/Fresnel/Profunctor/Coexp.hs
+++ src/Fresnel/Profunctor/Coexp.hs
@@ -7,6 +7,9 @@ import Data.Profunctor
 
 -- Coexponential
 
+-- | Coexponentials are the dual of functions, consisting of an argument of type @a@ (derived within an environment of type @e@) and a continuation from the return type @b@ (extending to the eventual result type @r@). As such, they naturally have the shape of optics, relating the outer context @e -> r@ to the inner @a -> b@.
+--
+-- The record selector names were chosen to indicate that 'Coexp' is essentially the pairing of 'Forget' and 'Fresnel.Profunctor.Recall'.
 data Coexp r e b a = Coexp { recall :: e -> a, forget :: b -> r }
   deriving (Functor)
 
