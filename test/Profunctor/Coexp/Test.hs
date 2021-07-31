24bcdf2a5e9b4a0131cd51209465a922659c5534
Rob Rix <rob.rix@me.com>
2021-07-31 10:57:57 -0400

    Test the associativity of the Semigroup instance for Coexp.

--- test/Profunctor/Coexp/Test.hs
+++ test/Profunctor/Coexp/Test.hs
@@ -1,6 +1,31 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Profunctor.Coexp.Test
 ( test
 ) where
 
-test :: IO ()
-test = pure ()
+import Fresnel.Profunctor.Coexp
+import Test.QuickCheck
+
+prop_semigroup_assoc :: (Eq b, Eq a, Show b, Show a) => ArbCoexp b a a b -> ArbCoexp b a a b -> ArbCoexp b a a b -> b -> a -> Property
+prop_semigroup_assoc a b c x y =
+  appCoexp (coexp a <> (coexp b <> coexp c)) x y === appCoexp ((coexp a <> coexp b) <> coexp c) x y
+
+
+data ArbCoexp e r a b = ArbCoexp (Fun e b) (Fun a r)
+  deriving (Show)
+
+coexp :: ArbCoexp e r a b -> Coexp e r a b
+coexp (ArbCoexp eb ar) = Coexp (applyFun eb) (applyFun ar)
+
+appCoexp :: Coexp e r a b -> e -> a -> (b, r)
+appCoexp (Coexp eb ar) e a = (eb e, ar a)
+
+
+instance (Function e, Function a, CoArbitrary e, CoArbitrary a, Arbitrary b, Arbitrary r) => Arbitrary (ArbCoexp e r a b) where
+  arbitrary = ArbCoexp <$> arbitrary <*> arbitrary
+
+
+pure []
+
+test :: IO Bool
+test = $quickCheckAll
