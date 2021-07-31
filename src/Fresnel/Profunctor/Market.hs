b932e9f03239eba14a874b35d6b9e01ed485a56b
Rob Rix <rob.rix@me.com>
2021-07-31 15:04:41 -0400

    Define a Functor instance for Market.

--- src/Fresnel/Profunctor/Market.hs
+++ src/Fresnel/Profunctor/Market.hs
@@ -10,5 +10,8 @@ import Data.Profunctor
 
 data Market a b s t = Market { inj :: b -> t, prj :: s -> Either t a }
 
+instance Functor (Market a b s) where
+  fmap = rmap
+
 instance Profunctor (Market a b) where
   dimap f g (Market inj prj) = Market (g . inj) (first g . prj . f)
