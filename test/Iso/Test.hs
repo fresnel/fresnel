b0171708a866b60059c6a5122397ba483da9ede7
Rob Rix <rob.rix@me.com>
2021-07-31 14:20:59 -0400

    Move validIso up so we can use it in this file.

--- test/Iso/Test.hs
+++ test/Iso/Test.hs
@@ -1,14 +1,18 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Iso.Test
-( test
-, validIso
+( validIso
+, test
 ) where
 
 import Fresnel.Getter
 import Fresnel.Iso
 import Fresnel.Review
 import Test.QuickCheck
+
+validIso :: (Eq a, Show a, Eq s, Show s) => Iso' s a -> s -> a -> Property
+validIso o s a = ((view o . review o) a === a) .&&. ((review o . view o) s === s)
+
 
 prop_view_elimination f g x = view (iso (applyFun f) (applyFun g)) x === applyFun f x
 
@@ -19,7 +23,3 @@ pure []
 
 test :: IO Bool
 test = $quickCheckAll
-
-
-validIso :: (Eq a, Show a, Eq s, Show s) => Iso' s a -> s -> a -> Property
-validIso o s a = ((view o . review o) a === a) .&&. ((review o . view o) s === s)
