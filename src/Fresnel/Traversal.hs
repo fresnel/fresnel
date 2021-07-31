12c8639bada8fc3016dbbed8bacf745143a67948
Rob Rix <rob.rix@me.com>
2021-07-31 08:44:19 -0400

    Define a Traversal type.

--- src/Fresnel/Traversal.hs
+++ src/Fresnel/Traversal.hs
@@ -1,2 +1,11 @@
 module Fresnel.Traversal
-() where
+( -- * Traversals
+  Traversal
+) where
+
+import Data.Profunctor.Traversing
+import Fresnel.Optic
+
+-- Traversals
+
+type Traversal s t a b = forall p . Traversing p => Optic p s t a b
