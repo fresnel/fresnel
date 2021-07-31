90d93e6cc49e124b8e9def48d6b2eb2b061940f5
Rob Rix <rob.rix@me.com>
2021-07-31 08:45:15 -0400

    Define a monomorphic-update Traversal type.

--- src/Fresnel/Traversal.hs
+++ src/Fresnel/Traversal.hs
@@ -1,6 +1,7 @@
 module Fresnel.Traversal
 ( -- * Traversals
   Traversal
+, Traversal'
 ) where
 
 import Data.Profunctor.Traversing
@@ -9,3 +10,5 @@ import Fresnel.Optic
 -- Traversals
 
 type Traversal s t a b = forall p . Traversing p => Optic p s t a b
+
+type Traversal' s a = Traversal s s a a
