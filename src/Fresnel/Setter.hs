bb293a6f40e3d9330d6898947204fb5ff1b2613a
Rob Rix <rob.rix@me.com>
2021-07-30 20:43:42 -0400

    Define a constructor for Setter.

--- src/Fresnel/Setter.hs
+++ src/Fresnel/Setter.hs
@@ -2,6 +2,8 @@ module Fresnel.Setter
 ( -- * Setters
   Setter
 , Setter'
+  -- * Construction
+, sets
 ) where
 
 import Data.Profunctor.Mapping
@@ -12,3 +14,9 @@ import Fresnel.Optic
 type Setter s t a b = forall p . Mapping p => Optic p s t a b
 
 type Setter' s a = Setter s s a a
+
+
+-- Construction
+
+sets :: ((a -> b) -> (s -> t)) -> Setter s t a b
+sets = roam
