a90eb52a7d5067d761e89f9d1072d028bab8a0bc
Rob Rix <rob.rix@me.com>
2021-07-30 20:16:49 -0400

    Alphabetize.

--- src/Fresnel/Profunctor/Recall.hs
+++ src/Fresnel/Profunctor/Recall.hs
@@ -24,16 +24,16 @@ instance Profunctor (Recall e) where
   dimap _ g = Recall . fmap g . runRecall
   rmap = fmap
 
-instance Costrong (Recall e) where
-  unfirst  = Recall . fmap fst . runRecall
-  unsecond = Recall . fmap snd . runRecall
-
 instance Choice (Recall e) where
   left'  = Recall . fmap Left  . runRecall
   right' = Recall . fmap Right . runRecall
 
 instance Closed (Recall e) where
   closed = Recall . fmap const . runRecall
+
+instance Costrong (Recall e) where
+  unfirst  = Recall . fmap fst . runRecall
+  unsecond = Recall . fmap snd . runRecall
 
 instance Sieve (Recall e) ((->) e) where
   sieve = const . runRecall
