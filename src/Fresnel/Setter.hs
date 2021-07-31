a54d701cdb32c421c9936c3c2b3ba5d2a8816ae3
Rob Rix <rob.rix@me.com>
2021-07-30 20:41:22 -0400

    Define a type of monomorphic-update Setters.

--- src/Fresnel/Setter.hs
+++ src/Fresnel/Setter.hs
@@ -1,6 +1,7 @@
 module Fresnel.Setter
 ( -- * Setters
   Setter
+, Setter'
 ) where
 
 import Data.Profunctor.Mapping
@@ -9,3 +10,5 @@ import Fresnel.Optic
 -- Setters
 
 type Setter s t a b = forall p . Mapping p => Optic p s t a b
+
+type Setter' s a = Setter s s a a
