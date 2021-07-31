3244589ba50422d634eef8009d86c0b1a191c743
Rob Rix <rob.rix@me.com>
2021-07-31 18:37:58 -0400

    Define the validity tests using withRoundtrips.

--- test/Iso/Test.hs
+++ test/Iso/Test.hs
@@ -5,8 +5,6 @@ module Iso.Test
 , validIso1
 , invalidIso
 , withRoundtrips
-, tripL
-, tripR
 , test
 ) where
 
@@ -16,22 +14,16 @@ import Fresnel.Review
 import Test.QuickCheck
 
 validIso :: (Eq a, Show a, Eq s, Show s) => Iso' s a -> s -> a -> Property
-validIso o s a = (tripR o a === a) .&&. (tripL o s === s)
+validIso o s a = withRoundtrips o $ \ ss aa -> (aa a === a) .&&. (ss s === s)
 
 validIso1 :: (Eq b, Show b) => Iso' (a -> b) b -> a -> b -> Property
-validIso1 o a b = (tripR o b === b) .&&. (tripL o (const b) a === b)
+validIso1 o a b = withRoundtrips o $ \ abab bb -> (bb b === b) .&&. (abab (const b) a === b)
 
 invalidIso :: (Eq a, Show a, Eq s, Show s) => Iso' s a -> s -> a -> Property
-invalidIso o s a = (tripR o a =/= a) .||. (tripL o s =/= s)
+invalidIso o s a = withRoundtrips o $ \ ss aa -> (aa a =/= a) .||. (ss s =/= s)
 
 withRoundtrips :: Iso' s a -> (((s -> s) -> (a -> a) -> r) -> r)
 withRoundtrips o k = withIso o (\ f g -> k (g . f) (f . g))
-
-tripL :: Iso' s a -> (s -> s)
-tripL o = withRoundtrips o const
-
-tripR :: Iso' s a -> (a -> a)
-tripR o = withRoundtrips o (const id)
 
 
 prop_view_elimination f g x = view (iso (applyFun f) (applyFun g)) x === applyFun f x
