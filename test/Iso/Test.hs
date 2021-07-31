d718bd1dca121a3865914c97b09cdf264ec8db1e
Rob Rix <rob.rix@me.com>
2021-07-31 14:19:22 -0400

    Define a property checking for Iso validity.

--- test/Iso/Test.hs
+++ test/Iso/Test.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Iso.Test
 ( test
+, validIso
 ) where
 
 import Fresnel.Getter
@@ -17,3 +19,7 @@ pure []
 
 test :: IO Bool
 test = $quickCheckAll
+
+
+validIso :: (Eq a, Show a, Eq s, Show s) => Iso' s a -> s -> a -> Property
+validIso o s a = ((view o . review o) a === a) .&&. ((review o . view o) s === s)
