51c7565ed9fbf359e7f78f852fab11a0acd08653
Rob Rix <rob.rix@me.com>
2021-08-01 11:34:57 -0400

    Construct folds from Foldable.

--- src/Fresnel/Fold.hs
+++ src/Fresnel/Fold.hs
@@ -2,8 +2,11 @@
 module Fresnel.Fold
 ( -- * Folds
   Fold
+  -- * Construction
+, folded
 ) where
 
+import Data.Foldable (traverse_)
 import Data.Profunctor
 import Data.Profunctor.Traversing
 import Fresnel.Bifunctor.Contravariant
@@ -12,3 +15,9 @@ import Fresnel.Optic
 -- Folds
 
 type Fold s a = forall p . (Bicontravariant p, Cochoice p, Traversing p) => Optic' p s a
+
+
+-- Construction
+
+folded :: Foldable f => Fold (f a) a
+folded = rphantom . wander traverse_
