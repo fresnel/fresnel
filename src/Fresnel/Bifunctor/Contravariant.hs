2017e1a33d132fec1a91854baec089195a8aef75
Rob Rix <rob.rix@me.com>
2021-08-01 11:15:27 -0400

    Headings.

--- src/Fresnel/Bifunctor/Contravariant.hs
+++ src/Fresnel/Bifunctor/Contravariant.hs
@@ -1,6 +1,9 @@
 module Fresnel.Bifunctor.Contravariant
-( Bicontravariant(..)
+( -- * Bicontravariant functors
+  Bicontravariant(..)
 ) where
+
+-- Bicontravariant functors
 
 class Bicontravariant p where
   contrabimap :: (a' -> a) -> (b' -> b) -> p a b -> p a' b'
