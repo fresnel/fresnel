4d14548bf41bf6f74ae724991588e802119c5f64
Rob Rix <rob.rix@me.com>
2021-08-29 12:24:38 -0400

    Introduce AffineFolds.

--- src/Fresnel/AffineFold.hs
+++ src/Fresnel/AffineFold.hs
@@ -2,6 +2,8 @@
 module Fresnel.AffineFold
 ( -- * Affine folds
   AffineFold
+  -- * Construction
+, afolding
 ) where
 
 import Data.Profunctor
@@ -13,3 +15,9 @@ import Fresnel.Optic
 
 -- FIXME: this is stronger than we actually mean; we need a Visiting class to express that.
 type AffineFold s a = forall p . (Bicontravariant p, Traversing p) => Optic' p s a
+
+
+-- Construction
+
+afolding :: (s -> Maybe a) -> AffineFold s a
+afolding f = contrabimap ((`maybe` Right) . Left <*> f) Left . right'
