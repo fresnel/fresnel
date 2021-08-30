3453688e8f970711ea66db78ebdcf3bae99f450c
Rob Rix <rob.rix@me.com>
2021-08-30 15:39:48 -0400

    Correct a doc comment.

--- src/Fresnel/Profunctor/Recall.hs
+++ src/Fresnel/Profunctor/Recall.hs
@@ -12,7 +12,7 @@ import Data.Profunctor
 import Data.Profunctor.Rep as Pro
 import Data.Profunctor.Sieve
 
--- * Recall profunctor
+-- Recall profunctor
 
 -- | @'Recall' e@ is dual to @'Forget' r@: it ignores the argument parameter, substituting in one of its own.
 newtype Recall e a b = Recall { runRecall :: e -> b }
