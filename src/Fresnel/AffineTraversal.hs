c9a2f462f67d6e369e025896de96f82038b268d7
Rob Rix <rob.rix@me.com>
2021-08-30 10:17:25 -0400

    Define a constructor for AffineTraversal.

--- src/Fresnel/AffineTraversal.hs
+++ src/Fresnel/AffineTraversal.hs
@@ -1,13 +1,28 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE LambdaCase #-}
 module Fresnel.AffineTraversal
 ( -- * Affine traversals
   AffineTraversal
 , AffineTraversing
+  -- * Construction
+, atraversal
 ) where
 
 import Fresnel.Profunctor.Optical
 import Fresnel.Optic
+import Data.Profunctor
 
 -- Affine traversals
 
 type AffineTraversal s t a b = forall p . AffineTraversing p => Optic p s t a b
+
+
+-- Construction
+
+atraversal :: (s -> Either t a) -> (s -> b -> t) -> AffineTraversal s t a b
+atraversal prj set = dimap
+  (\ s -> (prj s, set s))
+  (\case
+    (Left  t, _) -> t
+    (Right b, f) -> f b)
+  . first' . right'
