1151fb58d0a823f3142910e27256f9ce2524e6ff
Rob Rix <rob.rix@me.com>
2021-08-30 09:36:58 -0400

    Define a Reviewing class inheriting from Isoing.

--- src/Fresnel/Profunctor/Reviewing.hs
+++ src/Fresnel/Profunctor/Reviewing.hs
@@ -1,2 +1,12 @@
 module Fresnel.Profunctor.Reviewing
-() where
+( -- * Review constraints
+  Reviewing
+) where
+
+import Fresnel.Profunctor.Isoing
+import Data.Bifunctor
+import Data.Profunctor
+
+-- Review constraints
+
+class (Isoing p, Bifunctor p, Choice p) => Reviewing p
