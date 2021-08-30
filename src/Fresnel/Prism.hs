b63fc0b660669805f72b9ec4b808867d2afb4cd6
Rob Rix <rob.rix@me.com>
2021-08-30 10:05:01 -0400

    Define a Prisming instance for UnpackedPrism.

--- src/Fresnel/Prism.hs
+++ src/Fresnel/Prism.hs
@@ -101,6 +101,7 @@ instance Choice (UnpackedPrism a b) where
   left' (UnpackedPrism r) = r $ \ inj prj -> unpackedPrism (Left . inj) (either (either (Left . Left) Right . prj) (Left . Right))
 
 instance Isoing (UnpackedPrism a b)
+instance Prisming (UnpackedPrism a b)
 
 
 unpackedPrism :: (b -> t) -> (s -> Either t a) -> UnpackedPrism a b s t
