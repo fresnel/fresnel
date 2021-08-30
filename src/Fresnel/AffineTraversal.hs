7ab3475edf95294a33360f413804649ad14db91f
Rob Rix <rob.rix@me.com>
2021-08-30 13:17:15 -0400

    Define an atraverseOf eliminator for AffineTraversal.

--- src/Fresnel/AffineTraversal.hs
+++ src/Fresnel/AffineTraversal.hs
@@ -10,6 +10,7 @@ module Fresnel.AffineTraversal
   -- * Elimination
 , matching
 , withAffineTraversal
+, atraverseOf
   -- * Unpacked
 , UnpackedAffineTraversal(..)
 , unpackedAffineTraversal
@@ -44,6 +45,9 @@ matching o = withAffineTraversal o const
 withAffineTraversal :: AffineTraversal s t a b -> (((s -> Either t a) -> (s -> b -> t) -> r) -> r)
 withAffineTraversal o = withUnpackedAffineTraversal (o (unpackedAffineTraversal Right (const id)))
 
+atraverseOf :: Functor f => AffineTraversal s t a b -> (forall r . r -> f r) -> (a -> f b) -> (s -> f t)
+atraverseOf o point = runAffineStar . o . affineStar point
+
 
 -- Unpacked
 
@@ -66,3 +70,30 @@ instance AffineTraversing (UnpackedAffineTraversal a b)
 
 unpackedAffineTraversal :: (s -> Either t a) -> (s -> b -> t) -> UnpackedAffineTraversal a b s t
 unpackedAffineTraversal prj set = UnpackedAffineTraversal (\ k -> k prj set)
+
+
+newtype AffineStar f a b = AffineStar { withAffineStar :: forall r . ((forall x . x -> f x) -> (a -> f b) -> r) -> r }
+
+instance Functor f => Profunctor (AffineStar f) where
+  dimap f g = mapAffineStar (dimap f (fmap g))
+
+instance Functor f => Choice (AffineStar f) where
+  left' (AffineStar r) = AffineStar (\ k -> r (\ point f -> k point (either (fmap Left . f) (fmap Right . point))))
+
+instance Functor f => Strong (AffineStar f) where
+  first'  r = mapAffineStar (\ f (a, c) -> (,c) <$> f a) r
+  second' r = mapAffineStar (\ f (c, a) -> (c,) <$> f a) r
+
+instance Functor f => Isoing (AffineStar f)
+instance Functor f => Lensing (AffineStar f)
+instance Functor f => Prisming (AffineStar f)
+instance Functor f => AffineTraversing (AffineStar f)
+
+affineStar :: (forall x . x -> f x) -> (a -> f b) -> AffineStar f a b
+affineStar point f = AffineStar (\ k -> k point f)
+
+runAffineStar :: AffineStar f a b -> (a -> f b)
+runAffineStar a = withAffineStar a (\ _ f -> f)
+
+mapAffineStar :: ((a -> f b) -> (c -> f d)) -> (AffineStar f a b -> AffineStar f c d)
+mapAffineStar f (AffineStar r) = AffineStar (\ k -> r (\ point -> k point . f))
