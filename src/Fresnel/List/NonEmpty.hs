1108d8f8419c910fee849260b244ad723ab2d96f
Rob Rix <rob.rix@me.com>
2021-09-03 15:41:39 -0400

    Define a lens over the head of NonEmpty.

--- src/Fresnel/List/NonEmpty.hs
+++ src/Fresnel/List/NonEmpty.hs
@@ -1,2 +1,12 @@
 module Fresnel.List.NonEmpty
-() where
+( -- * Optics
+  head_
+) where
+
+import qualified Data.List.NonEmpty as NE
+import           Fresnel.Lens (Lens', lens)
+
+-- Optics
+
+head_ :: Lens' (NE.NonEmpty a) a
+head_ = lens NE.head (\ (_ NE.:|as) a' -> a' NE.:|as)
