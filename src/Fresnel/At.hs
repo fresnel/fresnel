0564404a99da625b614f3ed931ac9337a84dffb1
Rob Rix <rob.rix@me.com>
2021-09-03 17:32:21 -0400

    Define an At class.

--- src/Fresnel/At.hs
+++ src/Fresnel/At.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE RankNTypes #-}
 module Fresnel.At
-( -- * Indexable collections
-  module Fresnel.Ixed
+( -- * Updateable collections
+  At(..)
+  -- * Indexable collections
+, module Fresnel.Ixed
 ) where
 
 import Fresnel.Ixed
+import Fresnel.Lens
+
+class Ixed c => At c where
+  at :: Index c -> Lens' c (Maybe (IxValue c))
