9321ad1bc82f118bdc43965f9bad6bc091f46b3f
Rob Rix <rob.rix@me.com>
2021-09-03 17:38:07 -0400

    Define an At instance for IntSet.

--- src/Fresnel/At.hs
+++ src/Fresnel/At.hs
@@ -6,8 +6,13 @@ module Fresnel.At
 , module Fresnel.Ixed
 ) where
 
-import Fresnel.Ixed
-import Fresnel.Lens
+import           Control.Monad (guard)
+import qualified Data.IntSet as IntSet
+import           Fresnel.Ixed
+import           Fresnel.Lens (Lens', lens)
 
 class Ixed c => At c where
   at :: Index c -> Lens' c (Maybe (IxValue c))
+
+instance At IntSet.IntSet where
+  at k = lens (guard . IntSet.member k) (\ s -> maybe s (const (IntSet.insert k s)))
