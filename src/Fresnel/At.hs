9977bb8a5ff6f3d00a4101bf767974d53adffb8f
Rob Rix <rob.rix@me.com>
2021-09-03 09:25:29 -0400

    Define an ix method.

--- src/Fresnel/At.hs
+++ src/Fresnel/At.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 module Fresnel.At
 ( -- * Indexable collections
   Ixed(..)
 ) where
 
+import Fresnel.Traversal
+
 -- Indexable collections
 
 class Ixed c where
   type Index c
   type IxValue c
+
+  ix :: Index c -> Traversal' c (IxValue c)
