1f3cbf3b468e85e09c8c87d1959d98123e203231
Rob Rix <rob.rix@me.com>
2021-09-03 15:42:20 -0400

    Define a lens over the tail of NonEmpty.

--- src/Fresnel/List/NonEmpty.hs
+++ src/Fresnel/List/NonEmpty.hs
@@ -1,6 +1,7 @@
 module Fresnel.List.NonEmpty
 ( -- * Optics
   head_
+, tail_
 ) where
 
 import qualified Data.List.NonEmpty as NE
@@ -10,3 +11,6 @@ import           Fresnel.Lens (Lens', lens)
 
 head_ :: Lens' (NE.NonEmpty a) a
 head_ = lens NE.head (\ (_ NE.:|as) a' -> a' NE.:|as)
+
+tail_ :: Lens' (NE.NonEmpty a) [a]
+tail_ = lens NE.tail (\ (a NE.:|_) as' -> a NE.:|as')
