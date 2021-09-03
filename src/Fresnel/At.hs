0d0ea1364e24a959f7476d005424ee78856fd1a7
Rob Rix <rob.rix@me.com>
2021-09-03 17:39:52 -0400

    Define an At instance for IntMap.

--- src/Fresnel/At.hs
+++ src/Fresnel/At.hs
@@ -7,6 +7,7 @@ module Fresnel.At
 ) where
 
 import           Control.Monad (guard)
+import qualified Data.IntMap as IntMap
 import qualified Data.IntSet as IntSet
 import           Fresnel.Ixed
 import           Fresnel.Lens (Lens', lens)
@@ -16,3 +17,6 @@ class Ixed c => At c where
 
 instance At IntSet.IntSet where
   at k = lens (guard . IntSet.member k) (\ s -> maybe s (const (IntSet.insert k s)))
+
+instance At (IntMap.IntMap v) where
+  at k = lens (IntMap.lookup k) (\ m -> maybe m (flip (IntMap.insert k) m))
