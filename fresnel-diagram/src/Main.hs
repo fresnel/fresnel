84798c9817c3f18656ff5ce3fa1b8b5b9eb12897
Rob Rix <rob.rix@me.com>
2021-09-04 08:02:56 -0400

    Add attrs to the svg tag.

--- fresnel-diagram/src/Main.hs
+++ fresnel-diagram/src/Main.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main
 ( main
 ) where
 
 import Text.Blaze.Svg.Renderer.Pretty
-import Text.Blaze.Svg11
+import Text.Blaze.Svg11 as S
+import Text.Blaze.Svg11.Attributes as A
 
 main :: IO ()
-main = putStrLn (renderSvg (svg (do
-  style (toMarkup "@import url(https://cdn.rawgit.com/dreampulse/computer-modern-web-font/master/fonts.css);"))))
+main = putStrLn . renderSvg $ svg ! version "1.1" ! xmlns "http://www.w3.org/2000/svg" $ do
+  S.style (toMarkup ("@import url(https://cdn.rawgit.com/dreampulse/computer-modern-web-font/master/fonts.css);" :: String))
+
+xmlns = customAttribute "xmlns"
