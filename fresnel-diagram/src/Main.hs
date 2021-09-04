4f8cbaee60af7e5d3abd1ba3a8e421d8ce728b7a
Rob Rix <rob.rix@me.com>
2021-09-04 07:43:01 -0400

    Spit out an SVG.

--- fresnel-diagram/src/Main.hs
+++ fresnel-diagram/src/Main.hs
@@ -2,5 +2,9 @@ module Main
 ( main
 ) where
 
+import Text.Blaze.Svg.Renderer.Pretty
+import Text.Blaze.Svg11
+
 main :: IO ()
-main = pure ()
+main = putStrLn (renderSvg (svg (do
+  style (toMarkup "@import url(https://cdn.rawgit.com/dreampulse/computer-modern-web-font/master/fonts.css);"))))
