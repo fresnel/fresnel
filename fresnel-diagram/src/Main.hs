50cfdef0f81ee59808d5607fb955273a46bd7f9f
Rob Rix <rob.rix@me.com>
2021-09-04 08:03:37 -0400

    Add a viewbox.

--- fresnel-diagram/src/Main.hs
+++ fresnel-diagram/src/Main.hs
@@ -8,7 +8,7 @@ import Text.Blaze.Svg11 as S
 import Text.Blaze.Svg11.Attributes as A
 
 main :: IO ()
-main = putStrLn . renderSvg $ svg ! version "1.1" ! xmlns "http://www.w3.org/2000/svg" $ do
+main = putStrLn . renderSvg $ svg ! version "1.1" ! xmlns "http://www.w3.org/2000/svg" ! viewbox "-575 -50 1300 650" $ do
   S.style (toMarkup ("@import url(https://cdn.rawgit.com/dreampulse/computer-modern-web-font/master/fonts.css);" :: String))
 
 xmlns = customAttribute "xmlns"
