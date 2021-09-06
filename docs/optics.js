9601a4186b4449e1212c6bcb778e40781f07535c
Rob Rix <rob.rix@me.com>
2021-09-05 23:09:12 -0400

    getElementsById doesn’t exist but querySelectorAll does.

--- docs/optics.js
+++ docs/optics.js
@@ -1,4 +1,4 @@
-document.getElementsByClassName('vertex').forEach(v => {
+document.querySelectorAll('.vertex').forEach(v => {
   v.onmouseover = () => v.classList.add('hover');
   v.onmouseout = () => v.classList.remove('hover');
 });
