3ffcc6c4bf1417d5e77b20b718a9a5e29732744d
Rob Rix <rob.rix@me.com>
2021-09-06 09:41:26 -0400

    Filter out empty strings.

--- docs/optics.js
+++ docs/optics.js
@@ -1,5 +1,5 @@
 function ancestors(v) {
-  return v.getAttribute("data-ancestors")?.split(' ').map(i => document.getElementById(i)) || [];
+  return v.getAttribute("data-ancestors")?.split(' ').filter(i => i.length > 0).map(i => document.getElementById(i)) || [];
 }
 document.querySelectorAll('.vertex').forEach(v => {
   v.onmouseover = () => [v, ...ancestors(v)].forEach(e => e.classList.add('hover'));
