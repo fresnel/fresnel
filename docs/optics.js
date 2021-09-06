a67094008f8cc0580d1ac7b8722018dea9c2c3c9
Rob Rix <rob.rix@me.com>
2021-09-06 09:30:19 -0400

    Highlight ancestors on hover.

--- docs/optics.js
+++ docs/optics.js
@@ -1,4 +1,7 @@
+function ancestors(v) {
+  return v.getAttribute("data-ancestors")?.split(' ').map(i => document.getElementById(i)) || [];
+}
 document.querySelectorAll('.vertex').forEach(v => {
-  v.onmouseover = () => v.classList.add('hover');
-  v.onmouseout = () => v.classList.remove('hover');
+  v.onmouseover = () => [v, ...ancestors(v)].forEach(e => e.classList.add('hover'));
+  v.onmouseout = () => [v, ...ancestors(v)].forEach(e => e.classList.remove('hover'));
 });
