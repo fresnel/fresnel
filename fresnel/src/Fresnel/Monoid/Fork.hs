7574e2fefab5db00770952253f5e715cee8102f0
Rob Rix <rob.rix@me.com>
2021-09-10 15:23:48 -0400

    Define a Monoid instance for Fork.

--- fresnel/src/Fresnel/Monoid/Fork.hs
+++ fresnel/src/Fresnel/Monoid/Fork.hs
@@ -6,3 +6,6 @@ newtype Fork r a = Fork { runFork :: (r -> r -> r) -> (a -> r) -> r -> r }
 
 instance Semigroup (Fork r a) where
   Fork a1 <> Fork a2 = Fork (\ fork leaf nil -> a1 fork leaf nil `fork` a2 fork leaf nil)
+
+instance Monoid (Fork r a) where
+  mempty = Fork (\ _ _ nil -> nil)
