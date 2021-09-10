ed48798677e71a20f3aa16ed9bb75a9450775e04
Rob Rix <rob.rix@me.com>
2021-09-10 15:20:53 -0400

    Define a smart constructor for Cons.

--- fresnel/src/Fresnel/Monoid/Cons.hs
+++ fresnel/src/Fresnel/Monoid/Cons.hs
@@ -1,6 +1,10 @@
 module Fresnel.Monoid.Cons
-( Cons(..)
+( cons
+, Cons(..)
 ) where
+
+cons :: a -> Cons r a
+cons a = Cons (\ cons nil -> cons a nil)
 
 newtype Cons r a = Cons { runCons :: (a -> r -> r) -> r -> r }
 
