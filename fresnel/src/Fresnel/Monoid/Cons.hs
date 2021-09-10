e8688b6af32ae8981e12e5142ea553cd5358648b
Rob Rix <rob.rix@me.com>
2021-09-10 15:20:33 -0400

    Define a Semigroup instance for Cons.

--- fresnel/src/Fresnel/Monoid/Cons.hs
+++ fresnel/src/Fresnel/Monoid/Cons.hs
@@ -3,3 +3,6 @@ module Fresnel.Monoid.Cons
 ) where
 
 newtype Cons r a = Cons { runCons :: (a -> r -> r) -> r -> r }
+
+instance Semigroup (Cons r a) where
+  Cons a1 <> Cons a2 = Cons (\ cons -> a1 cons . a2 cons)
