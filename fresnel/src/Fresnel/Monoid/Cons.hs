cb3a847c3933696b5ebeef51f387b49587bcb2be
Rob Rix <rob.rix@me.com>
2021-09-10 15:20:39 -0400

    Define a Monoid instance for Cons.

--- fresnel/src/Fresnel/Monoid/Cons.hs
+++ fresnel/src/Fresnel/Monoid/Cons.hs
@@ -6,3 +6,6 @@ newtype Cons r a = Cons { runCons :: (a -> r -> r) -> r -> r }
 
 instance Semigroup (Cons r a) where
   Cons a1 <> Cons a2 = Cons (\ cons -> a1 cons . a2 cons)
+
+instance Monoid (Cons r a) where
+  mempty = Cons (\ _ nil -> nil)
