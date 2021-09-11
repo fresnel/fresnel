c501853ba8055fb1ebcae0e566a53699b48c61d1
Rob Rix <rob.rix@me.com>
2021-09-11 13:19:08 -0400

    Define a singleton constructor for Snoc.

--- fresnel/src/Fresnel/Monoid/Snoc.hs
+++ fresnel/src/Fresnel/Monoid/Snoc.hs
@@ -2,8 +2,16 @@
 module Fresnel.Monoid.Snoc
 ( -- * Snoc lists
   Snoc(..)
+  -- * Construction
+, singleton
 ) where
 
 -- Snoc lists
 
 newtype Snoc a = Snoc { runSnoc :: forall r . (r -> a -> r) -> r -> r }
+
+
+-- Construction
+
+singleton :: a -> Snoc a
+singleton a = Snoc (\ snoc nil -> snoc nil a)
