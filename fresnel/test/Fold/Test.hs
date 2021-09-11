384ce435f8d26e06c8e4ba79af1bfc9091004cf9
Rob Rix <rob.rix@me.com>
2021-09-11 07:11:02 -0400

    Test the left identity law for <>.

--- fresnel/test/Fold/Test.hs
+++ fresnel/test/Fold/Test.hs
@@ -12,6 +12,9 @@ import Test.QuickCheck
 prop_union_semigroup_assoc :: (Eq a, Show a) => ArbUnion a -> ArbUnion a -> ArbUnion a -> [a] -> Property
 prop_union_semigroup_assoc (ArbUnion a) (ArbUnion b) (ArbUnion c) as = foldMapOf (getUnion (a <> (b <> c))) (:[]) as === foldMapOf (getUnion ((a <> b) <> c)) (:[]) as
 
+prop_union_monoid_left_identity :: (Eq a, Show a) => ArbUnion a -> [a] -> Property
+prop_union_monoid_left_identity (ArbUnion a) as = foldMapOf (getUnion (mempty <> a)) (:[]) as === foldMapOf (getUnion a) (:[]) as
+
 
 newtype ArbUnion a = ArbUnion (Union [a] a)
 
