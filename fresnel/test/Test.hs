7e8f72563ab205073cfb3f2adb426e9d2b32052c
Rob Rix <rob.rix@me.com>
2021-09-11 15:27:30 -0400

    Draw the headings in magenta.

--- fresnel/test/Test.hs
+++ fresnel/test/Test.hs
@@ -3,12 +3,16 @@ module Main
 ) where
 
 import           Data.Bool (bool)
+import           Data.Colour.RGBSpace
+import           Data.Colour.RGBSpace.HSL
+import           Data.Colour.SRGB
 import           Data.Traversable (for)
 import qualified Fold.Test
 import qualified Getter.Test
 import qualified Iso.Test
 import qualified Monoid.Fork.Test
 import qualified Profunctor.Coexp.Test
+import           System.Console.ANSI
 import           System.Exit (exitFailure, exitSuccess)
 import           Test.QuickCheck
 
@@ -23,4 +27,10 @@ main = traverse (uncurry (runQuickCheckAll quickCheckResult))
   >>= bool exitFailure exitSuccess . and
 
 runQuickCheckAll :: (Property -> IO Result) -> String -> [(String, Property)] -> IO Bool
-runQuickCheckAll qc __FILE__ ps = and <$ putStrLn __FILE__ <*> for ps (\ (xs, p) -> isSuccess <$ putStrLn ("=== " ++ xs ++ " ===") <*> qc p <* putStrLn "")
+runQuickCheckAll qc __FILE__ ps = and <$ setSGR [setBold, setRGB (hsl 300 1 0.75)] <* putStrLn __FILE__ <* setSGR [] <*> for ps (\ (xs, p) -> isSuccess <$ putStrLn ("=== " ++ xs ++ " ===") <*> qc p <* putStrLn "")
+
+setRGB :: RGB Float -> SGR
+setRGB = SetRGBColor Foreground . uncurryRGB sRGB
+
+setBold :: SGR
+setBold = SetConsoleIntensity BoldIntensity
