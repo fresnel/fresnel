b00519ec26383375afe83e43d2466971121f5075
Rob Rix <rob.rix@me.com>
2021-09-11 13:32:19 -0400

    Quantify over r.

--- fresnel/src/Fresnel/Monoid/Fork.hs
+++ fresnel/src/Fresnel/Monoid/Fork.hs
@@ -1,15 +1,16 @@
+{-# LANGUAGE RankNTypes #-}
 module Fresnel.Monoid.Fork
 ( singleton
 , Fork(..)
 ) where
 
-singleton :: a -> Fork r a
+singleton :: a -> Fork a
 singleton a = Fork (\ _ leaf _ -> leaf a)
 
-newtype Fork r a = Fork { runFork :: (r -> r -> r) -> (a -> r) -> r -> r }
+newtype Fork a = Fork { runFork :: forall r . (r -> r -> r) -> (a -> r) -> r -> r }
 
-instance Semigroup (Fork r a) where
+instance Semigroup (Fork a) where
   Fork a1 <> Fork a2 = Fork (\ fork leaf nil -> a1 fork leaf nil `fork` a2 fork leaf nil)
 
-instance Monoid (Fork r a) where
+instance Monoid (Fork a) where
   mempty = Fork (\ _ _ nil -> nil)
