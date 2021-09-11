2309517326ceaf107d1ead00b5712a9992b15fd9
Rob Rix <rob.rix@me.com>
2021-09-11 13:18:13 -0400

    Define a Snoc type.

--- fresnel/src/Fresnel/Monoid/Snoc.hs
+++ fresnel/src/Fresnel/Monoid/Snoc.hs
@@ -1,2 +1,9 @@
+{-# LANGUAGE RankNTypes #-}
 module Fresnel.Monoid.Snoc
-() where
+( -- * Snoc lists
+  Snoc(..)
+) where
+
+-- Snoc lists
+
+newtype Snoc a = Snoc { runSnoc :: forall r . (r -> a -> r) -> r -> r }
