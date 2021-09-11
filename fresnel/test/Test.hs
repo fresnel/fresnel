0cb588a28ec7b2eefc4b10852c4ae4e5e1a89a36
Rob Rix <rob.rix@me.com>
2021-09-11 14:03:10 -0400

    Fail the overall tests on failed runs.

--- fresnel/test/Test.hs
+++ fresnel/test/Test.hs
@@ -2,17 +2,20 @@ module Main
 ( main
 ) where
 
+import           Data.Bool (bool)
 import qualified Fold.Test
 import qualified Getter.Test
 import qualified Iso.Test
 import qualified Monoid.Fork.Test
 import qualified Profunctor.Coexp.Test
+import           System.Exit (exitFailure, exitSuccess)
 
 main :: IO ()
-main = sequence_
+main = sequence
   [ Fold.Test.test
   , Getter.Test.test
   , Iso.Test.test
   , Monoid.Fork.Test.test
   , Profunctor.Coexp.Test.test
   ]
+  >>= bool exitFailure exitSuccess . and
