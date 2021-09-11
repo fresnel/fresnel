cf5b2f2a3e275c053e3eea369e8c53951569dc41
Rob Rix <rob.rix@me.com>
2021-09-11 06:42:31 -0400

    Sketch out a test of Union.

--- fresnel/test/Fold/Test.hs
+++ fresnel/test/Fold/Test.hs
@@ -1,9 +1,19 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Fold.Test
 ( test
 ) where
 
+import Fresnel.Fold
 import Test.QuickCheck
+
+prop_union_semigroup_assoc :: (Eq a, Show a) => [a] -> Property
+prop_union_semigroup_assoc as = foldMapOf (getUnion (union <> (union <> union))) (:[]) as === foldMapOf (getUnion ((union <> union) <> union)) (:[]) as
+
+
+union :: Union [a] a
+union = Union folded
+
 
 pure []
 
