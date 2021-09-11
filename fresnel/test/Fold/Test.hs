6e5cdbd7684f8a9c7c0170daaa9d97742f4166ab
Rob Rix <rob.rix@me.com>
2021-09-11 07:12:37 -0400

    Rename ArbUnion to ArbFold.

--- fresnel/test/Fold/Test.hs
+++ fresnel/test/Fold/Test.hs
@@ -9,26 +9,26 @@ import Fresnel.Fold
 import Fresnel.Ixed
 import Test.QuickCheck
 
-prop_union_semigroup_assoc :: (Eq a, Show a) => ArbUnion a -> ArbUnion a -> ArbUnion a -> [a] -> Property
-prop_union_semigroup_assoc (ArbUnion a) (ArbUnion b) (ArbUnion c) as = foldMapOf (getUnion (a <> (b <> c))) (:[]) as === foldMapOf (getUnion ((a <> b) <> c)) (:[]) as
+prop_union_semigroup_assoc :: (Eq a, Show a) => ArbFold a -> ArbFold a -> ArbFold a -> [a] -> Property
+prop_union_semigroup_assoc (ArbFold a) (ArbFold b) (ArbFold c) as = foldMapOf (getUnion (a <> (b <> c))) (:[]) as === foldMapOf (getUnion ((a <> b) <> c)) (:[]) as
 
-prop_union_monoid_left_identity :: (Eq a, Show a) => ArbUnion a -> [a] -> Property
-prop_union_monoid_left_identity (ArbUnion a) as = foldMapOf (getUnion (mempty <> a)) (:[]) as === foldMapOf (getUnion a) (:[]) as
+prop_union_monoid_left_identity :: (Eq a, Show a) => ArbFold a -> [a] -> Property
+prop_union_monoid_left_identity (ArbFold a) as = foldMapOf (getUnion (mempty <> a)) (:[]) as === foldMapOf (getUnion a) (:[]) as
 
-prop_union_monoid_right_identity :: (Eq a, Show a) => ArbUnion a -> [a] -> Property
-prop_union_monoid_right_identity (ArbUnion a) as = foldMapOf (getUnion (a <> mempty)) (:[]) as === foldMapOf (getUnion a) (:[]) as
+prop_union_monoid_right_identity :: (Eq a, Show a) => ArbFold a -> [a] -> Property
+prop_union_monoid_right_identity (ArbFold a) as = foldMapOf (getUnion (a <> mempty)) (:[]) as === foldMapOf (getUnion a) (:[]) as
 
 
-newtype ArbUnion a = ArbUnion (Union [a] a)
+newtype ArbFold a = ArbFold (Union [a] a)
 
-instance Show a => Show (ArbUnion a) where
-  showsPrec _ (ArbUnion (Union fold)) = showList (foldMapOf fold (:[]) []) -- FIXME: this is a bad instance
+instance Show a => Show (ArbFold a) where
+  showsPrec _ (ArbFold (Union fold)) = showList (foldMapOf fold (:[]) []) -- FIXME: this is a bad instance
 
-instance Arbitrary a => Arbitrary (ArbUnion a) where
+instance Arbitrary a => Arbitrary (ArbFold a) where
   arbitrary = oneof
-    [ pure (ArbUnion (Union folded))
-    , pure (ArbUnion (Union ignored))
-    , ArbUnion . ixed <$> arbitrary
+    [ pure (ArbFold (Union folded))
+    , pure (ArbFold (Union ignored))
+    , ArbFold . ixed <$> arbitrary
     ]
     where
     ixed :: Int -> Union [a] a
