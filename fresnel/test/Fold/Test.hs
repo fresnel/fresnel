fcc169a985f865a3c5fa0bc909ac44f6c4194170
Rob Rix <rob.rix@me.com>
2021-09-11 07:07:15 -0400

    Generate and test against arbitrary unions.

--- fresnel/test/Fold/Test.hs
+++ fresnel/test/Fold/Test.hs
@@ -1,18 +1,32 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Fold.Test
 ( test
 ) where
 
 import Fresnel.Fold
+import Fresnel.Ixed
 import Test.QuickCheck
 
-prop_union_semigroup_assoc :: (Eq a, Show a) => [a] -> Property
-prop_union_semigroup_assoc as = foldMapOf (getUnion (union <> (union <> union))) (:[]) as === foldMapOf (getUnion ((union <> union) <> union)) (:[]) as
+prop_union_semigroup_assoc :: (Eq a, Show a) => ArbUnion a -> ArbUnion a -> ArbUnion a -> [a] -> Property
+prop_union_semigroup_assoc (ArbUnion a) (ArbUnion b) (ArbUnion c) as = foldMapOf (getUnion (a <> (b <> c))) (:[]) as === foldMapOf (getUnion ((a <> b) <> c)) (:[]) as
 
 
-union :: Union [a] a
-union = Union folded
+newtype ArbUnion a = ArbUnion (Union [a] a)
+
+instance Show a => Show (ArbUnion a) where
+  showsPrec _ (ArbUnion (Union fold)) = showList (foldMapOf fold (:[]) []) -- FIXME: this is a bad instance
+
+instance Arbitrary a => Arbitrary (ArbUnion a) where
+  arbitrary = oneof
+    [ pure (ArbUnion (Union folded))
+    , pure (ArbUnion (Union ignored))
+    , ArbUnion . ixed <$> arbitrary
+    ]
+    where
+    ixed :: Int -> Union [a] a
+    ixed i = Union (ix i)
 
 
 pure []
