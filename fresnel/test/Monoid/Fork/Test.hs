5733322fbfd6c686b5bb187c24b1aa70afcd154c
Rob Rix <rob.rix@me.com>
2021-09-12 04:43:15 -0400

    Label the kinds of generated values.

--- fresnel/test/Monoid/Fork/Test.hs
+++ fresnel/test/Monoid/Fork/Test.hs
@@ -5,29 +5,36 @@ module Monoid.Fork.Test
 ) where
 
 import Data.Foldable (toList)
-import Fresnel.Monoid.Fork
+import Fresnel.Monoid.Fork (Fork, singleton)
 import Test.QuickCheck
 
 prop_semigroup_assoc :: (Eq a, Show a) => ArbFork a -> ArbFork a -> ArbFork a -> Property
-prop_semigroup_assoc (ArbFork a) (ArbFork b) (ArbFork c) =
-  toList (a <> (b <> c)) === toList ((a <> b) <> c)
+prop_semigroup_assoc (ArbFork ka a) (ArbFork kb b) (ArbFork kc c) =
+  label (show ka) . label (show kb) . label (show kc) $
+  (toList (a <> (b <> c)) === toList ((a <> b) <> c))
 
 prop_monoid_identity :: (Eq a, Show a) => ArbFork a -> Property
-prop_monoid_identity (ArbFork a) = toList (mempty <> a) === toList a .&&. toList (a <> mempty) === toList a
+prop_monoid_identity (ArbFork ka a) = label (show ka) $ toList (mempty <> a) === toList a .&&. toList (a <> mempty) === toList a
 
 
-newtype ArbFork a = ArbFork (Fork a)
+data ArbFork a = ArbFork Kind (Fork a)
   deriving (Show)
 
-instance Arbitrary a => Arbitrary (ArbFork a) where
-  arbitrary = ArbFork <$> sized go where
-    go 0 = pure mempty
-    go i = oneof
-      [ chooseInt (0, i) >>= \ j -> (<>) <$> go (i - j) <*> go j
-      , singleton <$> arbitrary
-      , pure mempty
-      ]
+data Kind = Nil | Leaf | Fork
+  deriving (Eq, Show)
 
+instance Arbitrary a => Arbitrary (ArbFork a) where
+  arbitrary = uncurry ArbFork <$> sized go where
+    go 0 = pure (Nil, mempty)
+    go i = oneof
+      [ do
+        j <- chooseInt (0, i)
+        (_, l) <- go (i - j)
+        (_, r) <- go j
+        pure (Fork, l <> r)
+      , (,) Leaf . singleton <$> arbitrary
+      , pure (Nil, mempty)
+      ]
 
 
 pure []
