57495768c0e6e57bc7447037f17306b1c9ba58ae
Rob Rix <rob.rix@me.com>
2021-09-12 04:50:23 -0400

    Label by folding.

--- fresnel/test/Monoid/Fork/Test.hs
+++ fresnel/test/Monoid/Fork/Test.hs
@@ -5,36 +5,36 @@ module Monoid.Fork.Test
 ) where
 
 import Data.Foldable (toList)
-import Fresnel.Monoid.Fork (Fork, singleton)
+import Fresnel.Monoid.Fork (Fork(runFork), singleton)
 import Test.QuickCheck
 
 prop_semigroup_assoc :: (Eq a, Show a) => ArbFork a -> ArbFork a -> ArbFork a -> Property
-prop_semigroup_assoc (ArbFork ka a) (ArbFork kb b) (ArbFork kc c) =
-  label (show ka) . label (show kb) . label (show kc) $
+prop_semigroup_assoc (ArbFork a) (ArbFork b) (ArbFork c) =
+  label (summarize a) . label (summarize b) . label (summarize c) $
   (toList (a <> (b <> c)) === toList ((a <> b) <> c))
 
 prop_monoid_identity :: (Eq a, Show a) => ArbFork a -> Property
-prop_monoid_identity (ArbFork ka a) = label (show ka) $ toList (mempty <> a) === toList a .&&. toList (a <> mempty) === toList a
+prop_monoid_identity (ArbFork a) = label (summarize a) $ toList (mempty <> a) === toList a .&&. toList (a <> mempty) === toList a
 
 
-data ArbFork a = ArbFork Kind (Fork a)
+newtype ArbFork a = ArbFork (Fork a)
   deriving (Show)
 
 data Kind = Nil | Leaf | Fork
   deriving (Eq, Show)
 
 instance Arbitrary a => Arbitrary (ArbFork a) where
-  arbitrary = uncurry ArbFork <$> sized go where
-    go 0 = pure (Nil, mempty)
+  arbitrary = ArbFork <$> sized go where
+    go 0 = pure mempty
     go i = oneof
-      [ do
-        j <- chooseInt (0, i)
-        (_, l) <- go (i - j)
-        (_, r) <- go j
-        pure (Fork, l <> r)
-      , (,) Leaf . singleton <$> arbitrary
-      , pure (Nil, mempty)
+      [ chooseInt (0, i) >>= \ j -> (<>) <$> go (i - j) <*> go j
+      , singleton <$> arbitrary
+      , pure mempty
       ]
+
+
+summarize :: Fork a -> String
+summarize r = runFork r (\ _ _ -> "fork") (const "leaf") "nil"
 
 
 pure []
