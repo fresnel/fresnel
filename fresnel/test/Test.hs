83b8eacef3ba77f7402ac67e6856b8237754d676
Rob Rix <rob.rix@me.com>
2021-09-13 05:54:26 -0400

    Take over test result formatting.

--- fresnel/test/Test.hs
+++ fresnel/test/Test.hs
@@ -1,15 +1,22 @@
+{-# LANGUAGE DisambiguateRecordFields #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module Main
 ( main
 ) where
 
+import           Control.Monad (unless)
 import           Data.Bool (bool)
 import           Data.Char (isSpace)
 import           Data.Colour.RGBSpace
 import           Data.Colour.RGBSpace.HSL
 import           Data.Colour.SRGB
+import           Data.Foldable (toList, traverse_)
+import           Data.List (intercalate, intersperse)
+import qualified Data.Map as Map
 import           Data.Traversable (for)
 import qualified Fold.Test
+import           GHC.Exception.Type (Exception(displayException))
 import qualified Getter.Test
 import qualified Iso.Test
 import qualified Monoid.Fork.Test
@@ -19,7 +26,7 @@ import           System.Exit (exitFailure, exitSuccess)
 import           Test.QuickCheck
 
 main :: IO ()
-main = traverse (uncurry (runQuickCheckAll (quickCheckWithResult stdArgs{ maxSuccess = 250 })))
+main = traverse (uncurry (runQuickCheckAll (quickCheckWithResult stdArgs{ maxSuccess = 250, chatty = False })))
   [ Fold.Test.tests
   , Getter.Test.tests
   , Iso.Test.tests
@@ -35,26 +42,92 @@ runQuickCheckAll qc __FILE__ ps = do
     putStrLn __FILE__
   putStrLn ""
   rs <- for ps $ \ (xs, p) -> do
-    pos <- getCursorPosition
-    let header colour = case breaks [isSpace, not . isSpace, isSpace, not . isSpace] xs of
-          [propName, _, _, _, loc] -> do
-            withSGR [setBold, setRGB colour] $
-              putStr (unwords (filter (\ s -> s /= "_" && s /= "prop") (breakAll (== '_') propName)))
-            putStrLn (' ' : '(' : loc ++ ")")
-          _ -> pure ()
-    header (hsl 180 1 0.35)
+    loc <- case breaks [isSpace, not . isSpace, isSpace, not . isSpace] xs of
+      [propName, _, _, _, loc] -> do
+        withSGR [setBold] $
+          putStrLn (unwords (filter (\ s -> s /= "_" && s /= "prop") (breakAll (== '_') propName)))
+        pure (Just loc)
+      _ -> pure Nothing
     r <- qc p
-    if isSuccess r then
-      pure ()
-    else do
-      saveCursor
-      maybe (pure ()) (uncurry setCursorPosition) pos
-      header (hsl 0 1 0.5)
-      restoreCursor
+    result loc r
     putStrLn ""
     pure (isSuccess r)
 
   tally (length (filter id rs), length (filter not rs))
+
+result :: Maybe String -> Result -> IO ()
+result loc = \case
+  Success{ numTests, numDiscarded, labels, classes, tables } -> do
+    success $ putStr "OK "
+    parens $ stats $ emptyStats{ Main.numTests, Main.numDiscarded, Main.labels, Main.classes, Main.tables }
+    putStrLn ""
+  GaveUp{ numTests, numDiscarded, labels, classes, tables } -> do
+    failure $ putStr "FAIL "
+    parens $ stats $ emptyStats{ Main.numTests, Main.numDiscarded, Main.labels, Main.classes, Main.tables }
+    putStrLn ""
+  Failure{ numTests, numDiscarded, numShrinks, usedSeed, usedSize, reason, theException, failingTestCase, failingLabels, failingClasses } -> do
+    maybe (pure ()) putStrLn loc
+    failure $ putStr "FAIL "
+    parens $ stats $ emptyStats{ Main.numTests, Main.numDiscarded, Main.numShrinks }
+    putStrLn ":"
+    putStrLn ""
+    putStrLn reason
+    maybe (pure ()) (putStrLn . displayException) theException
+    traverse_ putStrLn failingTestCase
+    putStrLn ""
+    putStrLn ("Seed: " ++ show usedSeed)
+    putStrLn ("Size: " ++ show usedSize)
+    unless (null failingLabels) $ putStrLn ("Labels: " ++ intercalate ", " failingLabels)
+    unless (null failingClasses) $ putStrLn ("Classes: " ++ intercalate ", " (toList failingClasses))
+  NoExpectedFailure{ numTests, numDiscarded, labels, classes, tables } -> do
+    failure $ putStr "FAIL "
+    parens $ stats $ emptyStats{ Main.numTests, Main.numDiscarded, Main.labels, Main.classes, Main.tables }
+    putStrLn ""
+
+data Stats = Stats
+  { numTests     :: Int
+  , numDiscarded :: Int
+  , numShrinks   :: Int
+  , labels       :: Map.Map [String] Int
+  , classes      :: Map.Map String Int
+  , tables       :: Map.Map String (Map.Map String Int)
+  }
+
+emptyStats :: Stats
+emptyStats = Stats
+  { numTests       = 0
+  , numDiscarded   = 0
+  , numShrinks     = 0
+  , labels         = Map.empty
+  , classes        = Map.empty
+  , tables         = Map.empty
+  }
+
+stats :: Stats -> IO ()
+stats Stats{ numTests, numDiscarded, numShrinks } = do
+  sequence_ . intersperse (putStr ", ")
+    $  toList (stat (S "test") numTests)
+    ++ toList (stat (S "discard") numDiscarded)
+    ++ toList (stat (S "shrink") numShrinks)
+
+data Plural
+  = S String
+  | C String String
+
+pluralize :: Int -> Plural -> String
+pluralize 1 = \case
+  S s   -> s
+  C s _ -> s
+pluralize _ = \case
+  S   s -> s ++ "s"
+  C _ s -> s
+
+stat :: Plural -> Int -> Maybe (IO ())
+stat _    0 = Nothing
+stat name n = Just $ do
+  putStr (show n)
+  putStr " "
+  putStr (pluralize n name)
 
 tally :: (Int, Int) -> IO (Int, Int)
 tally (successes, failures) = do
@@ -87,6 +160,13 @@ setRGB = SetRGBColor Foreground . uncurryRGB sRGB
 
 setBold :: SGR
 setBold = SetConsoleIntensity BoldIntensity
+
+
+parens :: IO a -> IO a
+parens m = do
+  putStr "("
+  a <- m
+  a <$ putStr ")"
 
 
 red :: RGB Float
