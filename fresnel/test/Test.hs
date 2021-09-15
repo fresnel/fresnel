69353eddd40546dc8920f6132ca33717069e1ac6
Rob Rix <rob.rix@me.com>
2021-09-15 18:10:38 -0400

    Indent by passing arguments around again.

--- fresnel/test/Test.hs
+++ fresnel/test/Test.hs
@@ -26,10 +26,11 @@ import           Test.QuickCheck (Args(..), Result(..), isSuccess, quickCheckWit
 import qualified Test.QuickCheck as QC
 
 main :: IO ()
-main = (`runIndentT` Indent []) $ do
-  res <- traverse (local (incr "  ") . runGroup stdArgs{ maxSuccess = 250, chatty = False } w) groups
-  (_, failures) <- uncurry tally (foldr (\ (s, f) (ss, fs) -> (s + ss, f + fs)) (0, 0) res)
-  lift $ if failures == 0 then
+main = do
+  let i = Indent ["  "]
+  res <- traverse (runGroup i stdArgs{ maxSuccess = 250, chatty = False } w) groups
+  (_, failures) <- uncurry (tally i) (foldr (\ (s, f) (ss, fs) -> (s + ss, f + fs)) (0, 0) res)
+  if failures == 0 then
     exitSuccess
   else
     exitFailure
@@ -50,40 +51,37 @@ newtype Indent = Indent { getIndent :: [String] }
 incr :: String -> Indent -> Indent
 incr i = Indent . (i:) . getIndent
 
-put :: String -> IndentT IO ()
-put = lift . putStr
+putNewline :: String -> IO ()
+putNewline s = putStr s *> newline
 
-putNewline :: String -> IndentT IO ()
-putNewline s = put s *> newline
-
-line :: IndentT IO a -> IndentT IO a
-line m = do
-  i <- asks (concat . reverse . getIndent)
-  put i
+line :: Indent -> IO a -> IO a
+line is m = do
+  let i = concat (reverse (getIndent is))
+  putStr i
   m
 
-lineStr :: String -> IndentT IO ()
-lineStr s = line $ put s *> newline
+lineStr :: Indent -> String -> IO ()
+lineStr i s = line i $ putStr s *> newline
 
-newline :: IndentT IO ()
-newline = lift (putStrLn "")
+newline :: IO ()
+newline = putStrLn ""
 
-runGroup :: Args -> Int -> Group -> IndentT IO (Int, Int)
-runGroup args width Group{ groupName, cases } = do
-  withSGR [setBold] $ lineStr groupName
-  lineStr (replicate (2 + fullWidth width) '━')
-  rs <- catMaybes <$> sequence (intersperse (Nothing <$ newline) (map (fmap Just <$> runCase args width) cases))
+runGroup :: Indent -> Args -> Int -> Group -> IO (Int, Int)
+runGroup i args width Group{ groupName, cases } = do
+  withSGR [setBold] $ lineStr i groupName
+  lineStr i (replicate (2 + fullWidth width) '━')
+  rs <- catMaybes <$> sequence (intersperse (Nothing <$ newline) (map (fmap Just <$> runCase i args width) cases))
   newline
-  tally (length (filter id rs)) (length (filter not rs))
+  tally i (length (filter id rs)) (length (filter not rs))
 
-runCase :: Args -> Int -> Case -> IndentT IO Bool
-runCase args width Case{ name, loc, property } = do
-  r <- lift (quickCheckWithResult args property)
-  result width name loc r
+runCase :: Indent -> Args -> Int -> Case -> IO Bool
+runCase i args width Case{ name, loc, property } = do
+  r <- quickCheckWithResult args property
+  result i width name loc r
   pure (isSuccess r)
 
-result :: Int -> String -> Loc -> Result -> IndentT IO ()
-result width name Loc{ path, lineNumber } res = case res of
+result :: Indent -> Int -> String -> Loc -> Result -> IO ()
+result i width name Loc{ path, lineNumber } res = case res of
   Success{ numTests, numDiscarded, labels, classes, tables } -> do
     header
     body numTests labels classes tables Stats{ Main.numTests, Main.numDiscarded, Main.numShrinks = 0 } "."
@@ -94,28 +92,28 @@ result width name Loc{ path, lineNumber } res = case res of
 
   Failure{ numTests, numDiscarded, numShrinks, usedSeed, usedSize, reason, theException, failingTestCase, failingLabels, failingClasses } -> do
     header
-    local (incr "│ ") $ do
-      stats Stats{ Main.numTests, Main.numDiscarded, Main.numShrinks }
-      unless (null failingClasses) $ put (" (" ++ intercalate ", " (toList failingClasses) ++ ")")
-      putNewline ":"
-      lineStr (path ++ ":" ++ show lineNumber)
-      lineStr reason
-      for_ theException (line . putNewline . displayException)
-      for_ failingTestCase (line . putNewline)
-      lineStr ""
-      lineStr ("Seed: " ++ show usedSeed)
-      lineStr ("Size: " ++ show usedSize)
-      unless (null failingLabels) . line . putNewline $ "Labels: "  ++ intercalate ", " failingLabels
+    i <- pure (incr "│ " i)
+    stats i Stats{ Main.numTests, Main.numDiscarded, Main.numShrinks }
+    unless (null failingClasses) $ putStr (" (" ++ intercalate ", " (toList failingClasses) ++ ")")
+    putNewline ":"
+    lineStr i (path ++ ":" ++ show lineNumber)
+    lineStr i reason
+    for_ theException (lineStr i . displayException)
+    for_ failingTestCase (lineStr i)
+    lineStr i ""
+    lineStr i ("Seed: " ++ show usedSeed)
+    lineStr i ("Size: " ++ show usedSize)
+    unless (null failingLabels) . lineStr i $ "Labels: "  ++ intercalate ", " failingLabels
 
   NoExpectedFailure{ numTests, numDiscarded, labels, classes, tables } -> do
     header
     body numTests labels classes tables Stats{ Main.numTests, Main.numDiscarded, Main.numShrinks = 0 } ":"
   where
   header = do
-    line $ do
+    line i $ do
       let δ = width - length name
-      withSGR [setBold] (put "❧ " *> put name *> when (width > 0) (put (replicate δ ' ')))
-      put "   "
+      withSGR [setBold] (putStr "❧ " *> putStr name *> when (width > 0) (putStr (replicate δ ' ')))
+      putStr "   "
       if succeeded then
         success $ putNewline "Success."
       else
@@ -127,12 +125,13 @@ result width name Loc{ path, lineNumber } res = case res of
         gutter
           | succeeded = "  "
           | otherwise = "╭─"
-    local (incr gutter) . line $
-      withSGR [setColour colour] $ putNewline (replicate (fullWidth width) '─')
+    i <- pure (incr gutter i)
+    line i $ withSGR [setColour colour] $ putNewline (replicate (fullWidth width) '─')
 
-  body numTests labels classes tables s t = local (incr "  ") $ do
-    sequence_ (intersperse (lineStr "") ((stats s *> Main.classes numTests classes *> putNewline t) : Main.labels numTests labels))
-    Main.tables numTests tables
+  body numTests labels classes tables s t = do
+    i <- pure (incr "  " i)
+    sequence_ (intersperse (lineStr i "") ((stats i s *> Main.classes i numTests classes *> putNewline t) : Main.labels i numTests labels))
+    Main.tables i numTests tables
 
   succeeded = case res of
     Success{} -> True
@@ -144,15 +143,15 @@ data Stats = Stats
   , numShrinks   :: Int
   }
 
-stats :: Stats -> IndentT IO ()
-stats Stats{ numTests, numDiscarded, numShrinks } = line . sequence_ . intersperse (put ", ")
+stats :: Indent -> Stats -> IO ()
+stats i Stats{ numTests, numDiscarded, numShrinks } = line i . sequence_ . intersperse (putStr ", ")
   $  toList (stat (S "test") numTests)
   ++ toList (stat (S "discard") numDiscarded)
   ++ toList (stat (S "shrink") numShrinks)
 
 
-labels :: Int -> Map.Map [String] Int -> [IndentT IO ()]
-labels n labels
+labels :: Indent -> Int -> Map.Map [String] Int -> [IO ()]
+labels i n labels
   | null labels = []
   | otherwise   = map (table n . sortBy (flip (comparing snd) <> flip (comparing fst)) . Map.toList) (IntMap.elems numberedLabels)
   where
@@ -163,18 +162,18 @@ labels n labels
     ]
   table k m = for_ m $ \ (key, v) -> do
     let percentage = fromIntegral v / fromIntegral k * 100 :: Double
-    line . putNewline $ (if percentage < 10 then " " else "") ++ showFFloatAlt (Just 1) percentage "" ++ "% " ++ key
+    lineStr i $ (if percentage < 10 then " " else "") ++ showFFloatAlt (Just 1) percentage "" ++ "% " ++ key
 
-classes :: Int -> Map.Map String Int -> IndentT IO ()
-classes n classes = unless (null classes) $ do
-  put " "
-  parens $ sequence_ (intersperse (put ", ") (map (uncurry (class_ n)) (Map.toList classes)))
+classes :: Indent -> Int -> Map.Map String Int -> IO ()
+classes i n classes = unless (null classes) $ do
+  putStr " "
+  parens $ sequence_ (intersperse (putStr ", ") (map (uncurry (class_ i n)) (Map.toList classes)))
 
-class_ :: Int -> String -> Int -> IndentT IO ()
-class_ n label n' = let percentage = fromIntegral n' / fromIntegral n * 100 :: Double in put (showFFloatAlt (Just 1) percentage ('%':' ':label))
+class_ :: Indent -> Int -> String -> Int -> IO ()
+class_ _ n label n' = let percentage = fromIntegral n' / fromIntegral n * 100 :: Double in putStr (showFFloatAlt (Just 1) percentage ('%':' ':label))
 
-tables :: Int -> Map.Map String (Map.Map String Int) -> IndentT IO ()
-tables _ _ = pure ()
+tables :: Indent -> Int -> Map.Map String (Map.Map String Int) -> IO ()
+tables _ _ _ = pure ()
 
 
 data Plural
@@ -192,24 +191,24 @@ pluralize _ = \case
 fullWidth :: Int -> Int
 fullWidth width = width + 3 + length "Success."
 
-stat :: Plural -> Int -> Maybe (IndentT IO ())
+stat :: Plural -> Int -> Maybe (IO ())
 stat _    0 = Nothing
 stat name n = Just $ do
-  put (show n)
-  put " "
-  put (pluralize n name)
+  putStr (show n)
+  putStr " "
+  putStr (pluralize n name)
 
-tally :: Int -> Int -> IndentT IO (Int, Int)
-tally successes failures = line $ do
+tally :: Indent -> Int -> Int -> IO (Int, Int)
+tally i successes failures = line i $ do
   let hasSuccesses = successes /= 0
       hasFailures = failures /= 0
   when hasSuccesses . success $ do
-    put (show successes)
-    put (' ' : pluralize successes (C "success" "successes"))
-  when (hasSuccesses && hasFailures) $ put ", "
+    putStr (show successes)
+    putStr (' ' : pluralize successes (C "success" "successes"))
+  when (hasSuccesses && hasFailures) $ putStr ", "
   when hasFailures . failure $ do
-    put (show failures)
-    put (' ' : pluralize failures (S "failure"))
+    putStr (show failures)
+    putStr (' ' : pluralize failures (S "failure"))
   when (hasSuccesses || hasFailures) (putNewline ".")
   newline
   pure (successes, failures)
@@ -221,48 +220,23 @@ setBold :: SGR
 setBold = SetConsoleIntensity BoldIntensity
 
 
-parens :: IndentT IO a -> IndentT IO a
+parens :: IO a -> IO a
 parens m = do
-  put "("
+  putStr "("
   a <- m
-  a <$ put ")"
+  a <$ putStr ")"
 
 
-withSGR :: [SGR] -> IndentT IO a -> IndentT IO a
-withSGR sgr io = lift (setSGR sgr) *> io <* lift (setSGR [])
+withSGR :: [SGR] -> IO a -> IO a
+withSGR sgr io = setSGR sgr *> io <* setSGR []
 
-colour :: Color -> IndentT IO a -> IndentT IO a
+colour :: Color -> IO a -> IO a
 colour c = withSGR [setColour c]
 
-success, failure :: IndentT IO a -> IndentT IO a
+success, failure :: IO a -> IO a
 
 success = colour Green
 failure = colour Red
-
-
-newtype IndentT m a = IndentT { runIndentT :: Indent -> m a }
-
-instance Functor m => Functor (IndentT m) where
-  fmap f = IndentT . fmap (fmap f) . runIndentT
-
-instance Applicative m => Applicative (IndentT m) where
-  pure = lift . pure
-  IndentT f <*> IndentT a = IndentT ((<*>) <$> f <*> a)
-
-instance Monad m => Monad (IndentT m) where
-  IndentT m >>= f = IndentT (\ i -> do
-    a <- m i
-    runIndentT (f a) i)
-
-lift :: m a -> IndentT m a
-lift = IndentT . const
-
-asks :: Applicative m => (Indent -> a) -> IndentT m a
-asks = IndentT . fmap pure
-
-local :: (Indent -> Indent) -> (IndentT m a -> IndentT m a)
-local f m = IndentT (runIndentT m . f)
-
 
 tropical :: Group
 tropical = Group
