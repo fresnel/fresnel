8fa4e001a0742b943f77e45fe31119fc8cb7debf
Rob Rix <rob.rix@me.com>
2021-09-29 11:43:56 -0400

    Test that review & unto are involutive.

--- fresnel/test/Review/Test.hs
+++ fresnel/test/Review/Test.hs
@@ -2,10 +2,13 @@
 module Review.Test
 ( tests
 ) where
+import Fresnel.Review
 import Language.Haskell.TH.Lib
 import Language.Haskell.TH.Syntax
 import Test.Group
 import Test.QuickCheck
+
+prop_review_unto_involution f x = review (unto (applyFun f)) x === applyFun f x
 
 
 pure []
