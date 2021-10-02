dbb1c34c93c72e9207d0d8a01226efa2ff2e08d9
Rob Rix <rob.rix@me.com>
2021-10-02 16:19:07 -0400

    Pull the tests up to the top level.

--- fresnel/test/Test.hs
+++ fresnel/test/Test.hs
@@ -15,19 +15,21 @@ import qualified Monoid.Fork.Test
 import qualified Profunctor.Coexp.Test
 import qualified Review.Test
 import           System.Environment (getArgs)
+import           Test.Group
 import           Test.Options
 import           Test.Run
 import qualified Tropical.Test
 
 main :: IO ()
-main = getArgs >>= withOptions defaultOpts (runEntries entries)
-  where
-  entries =
-    [ Fold.Test.tests
-    , Getter.Test.tests
-    , Iso.Test.tests
-    , Monoid.Fork.Test.tests
-    , Profunctor.Coexp.Test.tests
-    , Review.Test.tests
-    , Tropical.Test.tests
-    ]
+main = getArgs >>= withOptions defaultOpts (runEntries tests)
+
+tests :: [Entry]
+tests =
+  [ Fold.Test.tests
+  , Getter.Test.tests
+  , Iso.Test.tests
+  , Monoid.Fork.Test.tests
+  , Profunctor.Coexp.Test.tests
+  , Review.Test.tests
+  , Tropical.Test.tests
+  ]
