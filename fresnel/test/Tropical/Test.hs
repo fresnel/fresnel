1416ffc3c8b5b02bb5ced930add4f260bc818f54
Rob Rix <rob.rix@me.com>
2021-10-03 15:44:41 -0400

    Test the Tropical/semiring properties.

--- fresnel/test/Tropical/Test.hs
+++ fresnel/test/Tropical/Test.hs
@@ -3,16 +3,21 @@ module Tropical.Test
 ( tests
 ) where
 
+import Data.Monoid (Sum)
 import Fresnel.Tropical
 import Test.Group
-import Test.QuickCheck
+import Test.QuickCheck hiding ((><))
 
 prop_semigroup_assoc (ArbTropical a) (ArbTropical b) (ArbTropical c) = label (summarize a) . label (summarize b) . label (summarize c) $ a <> (b <> c) === (a <> b) <> c
 
 prop_monoid_identity (ArbTropical a) = label (summarize a) $ (mempty <> a) === a .&&. (a <> mempty) === a
 
+prop_semiring_assoc (ArbTropical a) (ArbTropical b) (ArbTropical c) = label (summarize a) . label (summarize b) . label (summarize c) $ a >< (b >< c) === (a >< b) >< c
 
-summarize :: Tropical Int -> String
+prop_unital_identity (ArbTropical a) = label (summarize a) $ (one >< a) === a .&&. (a >< one) === a
+
+
+summarize :: Tropical (Sum Int) -> String
 summarize (Tropical a) = case a of
   Nothing          -> "zero"
   Just a
@@ -21,7 +26,7 @@ summarize (Tropical a) = case a of
     | otherwise    -> "one"
 
 
-newtype ArbTropical = ArbTropical (Tropical Int)
+newtype ArbTropical = ArbTropical (Tropical (Sum Int))
   deriving (Eq, Ord, Show)
 
 instance Arbitrary ArbTropical where
