ec9dfd58c06d091f6ae002634d82e3c49f4fe793
Rob Rix <rob.rix@me.com>
2022-01-03 22:35:40 -0500

    Define an optic over the tail of a list.

--- fresnel/src/Fresnel/List.hs
+++ fresnel/src/Fresnel/List.hs
@@ -2,6 +2,7 @@ module Fresnel.List
 ( -- * Optics
   uncons_
 , head_
+, tail_
 ) where
 
 import qualified Data.List as List
@@ -15,3 +16,6 @@ uncons_ = prism' (uncurry (:)) List.uncons
 
 head_ :: Optional' [a] a
 head_ = uncons_.optional' (Just . fst) (\ (_,xs) x -> (x, xs))
+
+tail_ :: Optional' [a] [a]
+tail_ = uncons_.optional' (Just . snd) (\ (x, _) xs -> (x, xs))
