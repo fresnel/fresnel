d211885008ac9640d5a02b218d6397f0cf107e36
Rob Rix <rob.rix@me.com>
2022-01-03 21:06:45 -0500

    Define an optic over the head of a list.

--- fresnel/src/Fresnel/List.hs
+++ fresnel/src/Fresnel/List.hs
@@ -1,12 +1,17 @@
 module Fresnel.List
 ( -- * Optics
   uncons_
+, head_
 ) where
 
 import qualified Data.List as List
+import           Fresnel.Optional
 import           Fresnel.Prism
 
 -- Optics
 
 uncons_ :: Prism' [a] (a, [a])
 uncons_ = prism' (uncurry (:)) List.uncons
+
+head_ :: Optional' [a] a
+head_ = uncons_.optional' (Just . fst) (\ (_,xs) x -> (x, xs))
