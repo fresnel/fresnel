0aafda1ae651addd47f293db389aecd4d747cb24
Rob Rix <rob.rix@me.com>
2022-01-10 07:01:53 -0500

    Define an uncons_ Iso for NonEmpty.

--- fresnel/src/Fresnel/List/NonEmpty.hs
+++ fresnel/src/Fresnel/List/NonEmpty.hs
@@ -1,13 +1,18 @@
 module Fresnel.List.NonEmpty
 ( -- * Optics
-  head_
+  uncons_
+, head_
 , tail_
 ) where
 
 import qualified Data.List.NonEmpty as NE
+import           Fresnel.Iso (Iso', iso)
 import           Fresnel.Lens (Lens', lens)
 
 -- Optics
+
+uncons_ :: Iso' (NE.NonEmpty a) (a, Maybe (NE.NonEmpty a))
+uncons_ = iso NE.uncons (uncurry (NE.:|) . fmap (foldMap NE.toList))
 
 head_ :: Lens' (NE.NonEmpty a) a
 head_ = lens NE.head (\ (_ NE.:|as) a' -> a' NE.:|as)
