fb44bcfb270d0b4a8abcb1d9efaf10798683e5d3
Rob Rix <rob.rix@me.com>
2022-01-10 07:02:30 -0500

    uncons_ is polymorphic.

--- fresnel/src/Fresnel/List/NonEmpty.hs
+++ fresnel/src/Fresnel/List/NonEmpty.hs
@@ -6,12 +6,12 @@ module Fresnel.List.NonEmpty
 ) where
 
 import qualified Data.List.NonEmpty as NE
-import           Fresnel.Iso (Iso', iso)
+import           Fresnel.Iso (Iso, iso)
 import           Fresnel.Lens (Lens', lens)
 
 -- Optics
 
-uncons_ :: Iso' (NE.NonEmpty a) (a, Maybe (NE.NonEmpty a))
+uncons_ :: Iso (NE.NonEmpty a) (NE.NonEmpty b) (a, Maybe (NE.NonEmpty a)) (b, Maybe (NE.NonEmpty b))
 uncons_ = iso NE.uncons (uncurry (NE.:|) . fmap (foldMap NE.toList))
 
 head_ :: Lens' (NE.NonEmpty a) a
