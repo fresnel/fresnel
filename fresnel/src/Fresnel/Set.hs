2ae770c98bcde8f608602bc1feeea46cc5778b53
Rob Rix <rob.rix@me.com>
2022-02-12 18:08:18 -0500

    Map over sets.

--- fresnel/src/Fresnel/Set.hs
+++ fresnel/src/Fresnel/Set.hs
@@ -1,10 +1,15 @@
 {-# LANGUAGE RankNTypes #-}
 module Fresnel.Set
-( setOf
+( setmapped
+, setOf
 ) where
 
 import Data.Set as Set
 import Fresnel.Fold
+import Fresnel.Setter
+
+setmapped :: Ord b => Setter (Set a) (Set b) a b
+setmapped = sets Set.map
 
 setOf :: Ord a => Fold s a -> (s -> Set.Set a)
 setOf o = Set.fromList . toListOf o
