44694d59e137722c88f8b71931d2f671eedf104d
Rob Rix <rob.rix@me.com>
2022-02-26 17:59:09 -0500

    Define a view operation.

--- fresnel-fused-effects/src/Fresnel/Effect.hs
+++ fresnel-fused-effects/src/Fresnel/Effect.hs
@@ -1,14 +1,21 @@
 {-# LANGUAGE Rank2Types #-}
 module Fresnel.Effect
-( assign
+( view
+, assign
 , modifying
 , (.=)
 , (%=)
 ) where
 
 import           Control.Algebra
+import qualified Control.Effect.Reader as R
 import qualified Control.Effect.State as S
+import qualified Fresnel.Getter as O
 import qualified Fresnel.Setter as O
+
+view :: Has (R.Reader r) sig m => O.Getter r a -> m a
+view o = R.asks (O.view o)
+
 
 assign, (.=) :: Has (S.State s) sig m => O.Setter s s a b -> b -> m ()
 
