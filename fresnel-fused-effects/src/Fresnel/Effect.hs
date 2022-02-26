44bf2b29b0f800c2fabda2c574e69e6532cc1517
Rob Rix <rob.rix@me.com>
2022-02-26 17:49:19 -0500

    Add assign & its operator.

--- fresnel-fused-effects/src/Fresnel/Effect.hs
+++ fresnel-fused-effects/src/Fresnel/Effect.hs
@@ -1,2 +1,16 @@
+{-# LANGUAGE Rank2Types #-}
 module Fresnel.Effect
-() where
+( assign
+, (.=)
+) where
+
+import Control.Effect.State
+import Fresnel.Setter
+
+assign, (.=) :: Has (State s) sig m => Setter s s a b -> b -> m ()
+
+assign o v = modify (set o v)
+
+(.=) = assign
+
+infix 4 .=
