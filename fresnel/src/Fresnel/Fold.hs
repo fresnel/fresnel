966eefa0c4bf9d460b1cc25ab533ae8330f52a8d
Rob Rix <rob.rix@me.com>
2024-05-18 07:06:04 -0400

    Deal with negative inputs.

--- fresnel/src/Fresnel/Fold.hs
+++ fresnel/src/Fresnel/Fold.hs
@@ -85,7 +85,7 @@ filtered :: (a -> Bool) -> Fold a a
 filtered p = folding (\ a -> if p a then Just a else Nothing)
 
 replicated :: Int -> Fold a a
-replicated n0 = rphantom . wander (\ f -> let loop n a = if n == 0 then pure () else f a *> loop (n - 1) a in loop n0)
+replicated n0 = rphantom . wander (\ f -> let loop n a = if n <= 0 then pure () else f a *> loop (n - 1) a in loop n0)
 
 
 -- Elimination
