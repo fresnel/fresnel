ed8304a79a1bea13774dfcc080e1724ecc3b287e
Rob Rix <rob.rix@me.com>
2024-09-19 06:30:00 -0700

    Test ignored.

--- fresnel/test/Traversal/Test.hs
+++ fresnel/test/Traversal/Test.hs
@@ -3,7 +3,12 @@ module Traversal.Test
 ( tests
 ) where
 
+import Fresnel.Traversal
 import Test.Group
+import Test.QuickCheck
+
+prop_ignored (Fn f) a = traverseOf ignored f a === [a]
+
 
 pure []
 
