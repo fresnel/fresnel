d3688a33f434f1cfed5db2314b275dc823700e51
Rob Rix <rob.rix@me.com>
2024-09-21 07:45:36 -0400

    Relevant folds.

--- fresnel/src/Fresnel/RelevantFold.hs
+++ fresnel/src/Fresnel/RelevantFold.hs
@@ -1,2 +1,11 @@
+{-# LANGUAGE RankNTypes #-}
 module Fresnel.RelevantFold
-() where
+( RelevantFold
+) where
+
+import Fresnel.Optic
+import Fresnel.Traversal.Internal (IsTraversal)
+
+type RelevantFold s a = forall p . IsRelevantFold p => Optic' p s a
+
+class IsTraversal p => IsRelevantFold p
