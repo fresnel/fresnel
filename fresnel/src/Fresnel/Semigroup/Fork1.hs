5bcdf4fb9861d67bde245db7468859c2d1663d38
Rob Rix <rob.rix@me.com>
2024-09-21 20:44:55 -0400

    Show.

--- fresnel/src/Fresnel/Semigroup/Fork1.hs
+++ fresnel/src/Fresnel/Semigroup/Fork1.hs
@@ -4,9 +4,14 @@ module Fresnel.Semigroup.Fork1
   Fork1(..)
 ) where
 
+import Data.Foldable (toList)
+
 -- Non-empty binary trees
 
 newtype Fork1 a = Fork1 { runFork1 :: forall r . (r -> r -> r) -> (a -> r) -> r }
+
+instance Show a => Show (Fork1 a) where
+  showsPrec _ = showList . toList
 
 instance Foldable Fork1 where
   foldMap f (Fork1 r) = r (<>) f
