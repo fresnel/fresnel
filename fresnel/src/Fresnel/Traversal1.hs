8b54050766c749bb4086ee3d3d9a33821cc36f5a
Rob Rix <rob.rix@me.com>
2024-09-21 20:27:37 -0400

    Haddock sections.

--- fresnel/src/Fresnel/Traversal1.hs
+++ fresnel/src/Fresnel/Traversal1.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE RankNTypes #-}
 module Fresnel.Traversal1
-( Traversal1
+( -- * Relevant traversals
+  Traversal1
 , IsTraversal1
 ) where
 
 import Fresnel.Optic
 import Fresnel.Traversal1.Internal
+
+-- Relevant traversals
 
 type Traversal1 s t a b = forall p . IsTraversal1 p => Optic p s t a b
