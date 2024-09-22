a99205db15d142bdf1d63d6ab1fea47cfc65f3f9
Rob Rix <rob.rix@me.com>
2024-09-21 21:00:39 -0400

    Star IsFold1.

--- fresnel/src/Fresnel/Fold1/Internal.hs
+++ fresnel/src/Fresnel/Fold1/Internal.hs
@@ -2,10 +2,13 @@ module Fresnel.Fold1.Internal
 ( IsFold1
 ) where
 
-import Data.Profunctor (Forget)
+import Data.Functor.Apply
+import Data.Functor.Contravariant
+import Data.Profunctor (Forget, Star)
 import Fresnel.Getter.Internal (IsGetter)
 import Fresnel.Traversal1.Internal (IsTraversal1)
 
 class (IsGetter p, IsTraversal1 p) => IsFold1 p
 
 instance Semigroup r => IsFold1 (Forget r)
+instance (Contravariant f, Apply f, Traversable f) => IsFold1 (Star f)
