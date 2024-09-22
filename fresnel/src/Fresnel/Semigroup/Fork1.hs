7d270c0d36f7b3b60f0474269165014518926ebc
Rob Rix <rob.rix@me.com>
2024-09-21 20:44:21 -0400

    Fork1 is Foldable.

--- fresnel/src/Fresnel/Semigroup/Fork1.hs
+++ fresnel/src/Fresnel/Semigroup/Fork1.hs
@@ -7,3 +7,6 @@ module Fresnel.Semigroup.Fork1
 -- Non-empty binary trees
 
 newtype Fork1 a = Fork1 { runFork1 :: forall r . (r -> r -> r) -> (a -> r) -> r }
+
+instance Foldable Fork1 where
+  foldMap f (Fork1 r) = r (<>) f
