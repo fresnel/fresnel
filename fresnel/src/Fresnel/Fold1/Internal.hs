90881efffb2061cbab62191434bcd74dc97a3597
Rob Rix <rob.rix@me.com>
2024-09-21 21:00:12 -0400

    Forget IsFold1.

--- fresnel/src/Fresnel/Fold1/Internal.hs
+++ fresnel/src/Fresnel/Fold1/Internal.hs
@@ -2,7 +2,10 @@ module Fresnel.Fold1.Internal
 ( IsFold1
 ) where
 
+import Data.Profunctor (Forget)
 import Fresnel.Getter.Internal (IsGetter)
 import Fresnel.Traversal1.Internal (IsTraversal1)
 
 class (IsGetter p, IsTraversal1 p) => IsFold1 p
+
+instance Semigroup r => IsFold1 (Forget r)
