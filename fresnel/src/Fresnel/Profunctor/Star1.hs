79ce3cff5ed9c596a642c8ba42dc985795178ee9
Rob Rix <rob.rix@me.com>
2024-09-22 15:21:12 -0400

    Star1.

--- fresnel/src/Fresnel/Profunctor/Star1.hs
+++ fresnel/src/Fresnel/Profunctor/Star1.hs
@@ -1,2 +1,7 @@
 module Fresnel.Profunctor.Star1
-() where
+( -- * Star1 profunctor
+  Star1(..)
+) where
+
+-- | Just like 'Data.Profunctor.Star', but with some instances defined in terms of 'Data.Functor.Apply' instead of 'Applicative'. Used by 'Fresnel.Traversal1.Traversal1' & 'Fresnel.Fold1.Fold1'.
+newtype Star1 f a b = Star1 { runStar1 :: a -> f b }
