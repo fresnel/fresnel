6a588026402bbe57179f7dd87f597efadf6902a7
Rob Rix <rob.rix@me.com>
2024-09-22 15:33:49 -0400

    Star1 supports IsIso.

--- fresnel/src/Fresnel/Iso/Internal.hs
+++ fresnel/src/Fresnel/Iso/Internal.hs
@@ -7,6 +7,7 @@ import Data.Profunctor
 import Fresnel.Profunctor.Coexp (Coexp)
 import Fresnel.Profunctor.OptionalStar (OptionalStar)
 import Fresnel.Profunctor.Recall (Recall)
+import Fresnel.Profunctor.Star1 (Star1)
 
 class Profunctor p => IsIso p
 
@@ -17,4 +18,5 @@ instance IsIso (Recall e)
 instance Functor f => IsIso (Star f)
 instance Functor f => IsIso (Costar f)
 instance Functor f => IsIso (OptionalStar f)
+instance Functor f => IsIso (Star1 f)
 instance IsIso (Coexp s t)
