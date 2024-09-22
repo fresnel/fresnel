77eafd010f31354504497465fed94c1a16f03936
Rob Rix <rob.rix@me.com>
2024-09-22 14:04:13 -0400

    Foldable.

--- fresnel/src/Fresnel/Semigroup/Snoc1.hs
+++ fresnel/src/Fresnel/Semigroup/Snoc1.hs
@@ -7,3 +7,7 @@ module Fresnel.Semigroup.Snoc1
 -- Non-empty snoc lists
 
 newtype Snoc1 a = Snoc1 { runSnoc1 :: forall r . (a -> r) -> (r -> a -> r) -> r }
+
+instance Foldable Snoc1 where
+  foldMap f (Snoc1 r) = r f ((. f) . (<>))
+  foldl f z (Snoc1 r) = r (z `f`) f
