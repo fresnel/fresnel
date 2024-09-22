aa27c3e4df49db8c457a987827881ecced37524d
Rob Rix <rob.rix@me.com>
2024-09-22 13:51:30 -0400

    Foldable.

--- fresnel/src/Fresnel/Semigroup/Cons1.hs
+++ fresnel/src/Fresnel/Semigroup/Cons1.hs
@@ -7,3 +7,7 @@ module Fresnel.Semigroup.Cons1
 -- Non-empty cons lists
 
 newtype Cons1 a = Cons1 { runCons1 :: forall r . (a -> r) -> (a -> r -> r) -> r }
+
+instance Foldable Cons1 where
+  foldMap f (Cons1 r) = r f ((<>) . f)
+  foldr f z (Cons1 r) = r (`f` z) f
