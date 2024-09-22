179bc52f5513b57decbec1b8a37ddbb03148514a
Rob Rix <rob.rix@me.com>
2024-09-21 21:28:52 -0400

    Traversed1.

--- fresnel/src/Fresnel/Functor/Traversed1.hs
+++ fresnel/src/Fresnel/Functor/Traversed1.hs
@@ -1,2 +1,16 @@
 module Fresnel.Functor.Traversed1
-() where
+( -- * Traversed1 functor
+  runTraversed1
+, Traversed1(..)
+) where
+
+import Data.Functor (void)
+import Data.Functor.Apply
+
+runTraversed1 :: Functor f => Traversed1 f a -> f ()
+runTraversed1 (Traversed1 fa) = void fa
+
+newtype Traversed1 f a = Traversed1 (f a)
+
+instance Apply f => Semigroup (Traversed1 f a) where
+  Traversed1 a1 <> Traversed1 a2 = Traversed1 (a1 .> a2)
