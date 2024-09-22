1c2caccaa6a25ebdb654eb3210d3ef05c807e8c9
Rob Rix <rob.rix@me.com>
2024-09-22 15:39:00 -0400

    Star1 supports IsGetter.

--- fresnel/src/Fresnel/Getter/Internal.hs
+++ fresnel/src/Fresnel/Getter/Internal.hs
@@ -7,9 +7,11 @@ import Data.Profunctor (Cochoice, Forget, Star)
 import Fresnel.Bifunctor.Contravariant (Bicontravariant)
 import Fresnel.Lens.Internal (IsLens)
 import Fresnel.Profunctor.OptionalStar (OptionalStar)
+import Fresnel.Profunctor.Star1 (Star1)
 
 class (IsLens p, Bicontravariant p, Cochoice p) => IsGetter p
 
 instance IsGetter (Forget r)
 instance (Contravariant f, Traversable f) => IsGetter (Star f)
+instance (Contravariant f, Traversable f) => IsGetter (Star1 f)
 instance (Contravariant f, Traversable f) => IsGetter (OptionalStar f)
