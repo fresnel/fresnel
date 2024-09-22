171771f09822dbbf1ab5aea8a0ecffd174ca04a7
Rob Rix <rob.rix@me.com>
2024-09-21 21:08:16 -0400

    Use traversal1.

--- fresnel/src/Fresnel/Fold1.hs
+++ fresnel/src/Fresnel/Fold1.hs
@@ -10,7 +10,7 @@ import Data.Semigroup.Foldable
 import Fresnel.Bifunctor.Contravariant
 import Fresnel.Fold1.Internal (IsFold1)
 import Fresnel.Optic (Optic')
-import Fresnel.Profunctor.Traversing1
+import Fresnel.Traversal1
 
 -- Relevant folds
 
@@ -20,4 +20,4 @@ type Fold1 s a = forall p . IsFold1 p => Optic' p s a
 -- Construction
 
 folded1 :: Foldable1 t => Fold1 (t a) a
-folded1 = rphantom . wander1 traverse1_
+folded1 = rphantom . traversal1 traverse1_
