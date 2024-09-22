e7af283b541252ce4df1cc2567de664367c8443e
Rob Rix <rob.rix@me.com>
2024-09-21 20:28:07 -0400

    Monomorphic relevant traversals.

--- fresnel/src/Fresnel/Traversal1.hs
+++ fresnel/src/Fresnel/Traversal1.hs
@@ -2,6 +2,7 @@
 module Fresnel.Traversal1
 ( -- * Relevant traversals
   Traversal1
+, Traversal1'
 , IsTraversal1
 ) where
 
@@ -11,3 +12,5 @@ import Fresnel.Traversal1.Internal
 -- Relevant traversals
 
 type Traversal1 s t a b = forall p . IsTraversal1 p => Optic p s t a b
+
+type Traversal1' s a = Traversal1 s s a a
