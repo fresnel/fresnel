cf39e46d293e3181513ecaa44a9652e0eef5fd22
Rob Rix <rob.rix@me.com>
2024-09-21 20:14:39 -0400

    dimap via wander1.

--- fresnel/src/Fresnel/Profunctor/Traversing1.hs
+++ fresnel/src/Fresnel/Profunctor/Traversing1.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE RankNTypes #-}
 module Fresnel.Profunctor.Traversing1
 ( Traversing1(..)
+  -- ** Profunctor from Traversing1
+, dimapTraversing1
 ) where
 
 import Data.Functor.Apply
@@ -8,3 +10,9 @@ import Data.Profunctor.Strong
 
 class Strong p => Traversing1 p where
   wander1 :: (forall f . Apply f => (a -> f b) -> (s -> f t)) -> (p a b -> p s t)
+
+
+-- Profunctor from Traversing1
+
+dimapTraversing1 :: Traversing1 p => (a' -> a) -> (b -> b') -> (p a b -> p a' b')
+dimapTraversing1 f g = wander1 (\ k -> fmap g . k . f)
