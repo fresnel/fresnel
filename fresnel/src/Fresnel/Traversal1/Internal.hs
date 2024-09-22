37d89e00d7a0868224b4b4afa6d89a9a27e28f43
Rob Rix <rob.rix@me.com>
2024-09-22 15:34:39 -0400

    Star1 supports IsTraversal1.

--- fresnel/src/Fresnel/Traversal1/Internal.hs
+++ fresnel/src/Fresnel/Traversal1/Internal.hs
@@ -7,6 +7,7 @@ import Data.Functor.Apply
 import Data.Profunctor (Forget, Star)
 import Fresnel.Lens.Internal (IsLens)
 import Fresnel.Profunctor.OptionalStar (OptionalStar)
+import Fresnel.Profunctor.Star1 (Star1)
 import Fresnel.Profunctor.Traversing1 (Traversing1)
 
 class (IsLens p, Traversing1 p) => IsTraversal1 p
@@ -15,4 +16,5 @@ instance IsTraversal1 (->)
 instance Monad m => IsTraversal1 (Kleisli m)
 instance Semigroup r => IsTraversal1 (Forget r)
 instance Apply f => IsTraversal1 (Star f)
+instance Apply f => IsTraversal1 (Star1 f)
 instance Apply f => IsTraversal1 (OptionalStar f)
