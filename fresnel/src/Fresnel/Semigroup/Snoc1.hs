c548132458454c60fb01ce83137a47f698638eed
Rob Rix <rob.rix@me.com>
2024-09-22 14:02:13 -0400

    Snoc1.

--- fresnel/src/Fresnel/Semigroup/Snoc1.hs
+++ fresnel/src/Fresnel/Semigroup/Snoc1.hs
@@ -1,2 +1,9 @@
+{-# LANGUAGE RankNTypes #-}
 module Fresnel.Semigroup.Snoc1
-() where
+( -- * Non-empty snoc lists
+  Snoc1(..)
+) where
+
+-- Non-empty snoc lists
+
+newtype Snoc1 a = Snoc1 { runSnoc1 :: forall r . (a -> r) -> (r -> a -> r) -> r }
