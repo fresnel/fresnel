f1a545361a2a3ba7e4b735a6385d7d73f4b2e88f
Rob Rix <rob.rix@me.com>
2024-09-21 21:01:18 -0400

    OptionalStar IsFold1.

--- fresnel/src/Fresnel/Fold1/Internal.hs
+++ fresnel/src/Fresnel/Fold1/Internal.hs
@@ -6,9 +6,11 @@ import Data.Functor.Apply
 import Data.Functor.Contravariant
 import Data.Profunctor (Forget, Star)
 import Fresnel.Getter.Internal (IsGetter)
+import Fresnel.Profunctor.OptionalStar (OptionalStar)
 import Fresnel.Traversal1.Internal (IsTraversal1)
 
 class (IsGetter p, IsTraversal1 p) => IsFold1 p
 
 instance Semigroup r => IsFold1 (Forget r)
 instance (Contravariant f, Apply f, Traversable f) => IsFold1 (Star f)
+instance (Contravariant f, Apply f, Traversable f) => IsFold1 (OptionalStar f)
