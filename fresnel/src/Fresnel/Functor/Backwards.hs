fd0e4510270f864ff144aee2a42a4adde2056906
Rob Rix <rob.rix@me.com>
2024-09-22 15:43:25 -0400

    :fire: Backwards.

--- fresnel/src/Fresnel/Functor/Backwards.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Fresnel.Functor.Backwards
-( Backwards(..)
-) where
-
-import Data.Functor.Apply (Apply)
-
-newtype Backwards f a = Backwards { forwards :: f a }
-  deriving (Applicative, Apply, Functor)
