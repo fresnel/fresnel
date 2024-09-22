14d25c93b0f7c2f33384e5e62117342d3c05f93a
Rob Rix <rob.rix@me.com>
2024-09-21 20:13:55 -0400

    Traversing1.

--- fresnel/src/Fresnel/Profunctor/Traversing1.hs
+++ fresnel/src/Fresnel/Profunctor/Traversing1.hs
@@ -1,2 +1,10 @@
+{-# LANGUAGE RankNTypes #-}
 module Fresnel.Profunctor.Traversing1
-() where
+( Traversing1(..)
+) where
+
+import Data.Functor.Apply
+import Data.Profunctor.Strong
+
+class Strong p => Traversing1 p where
+  wander1 :: (forall f . Apply f => (a -> f b) -> (s -> f t)) -> (p a b -> p s t)
