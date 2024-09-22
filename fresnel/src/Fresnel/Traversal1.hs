1a4349420b386fc3bb7f2e0a6e9fc88180498424
Rob Rix <rob.rix@me.com>
2024-09-21 20:30:50 -0400

    Lift Traversable1 into Traversal1.

--- fresnel/src/Fresnel/Traversal1.hs
+++ fresnel/src/Fresnel/Traversal1.hs
@@ -6,9 +6,11 @@ module Fresnel.Traversal1
 , IsTraversal1
   -- * Construction
 , traversal1
+, traversed1
 ) where
 
 import Data.Functor.Apply
+import Data.Semigroup.Traversable
 import Fresnel.Optic
 import Fresnel.Profunctor.Traversing1
 import Fresnel.Traversal1.Internal
@@ -24,3 +26,6 @@ type Traversal1' s a = Traversal1 s s a a
 
 traversal1 :: (forall f . Apply f => (a -> f b) -> (s -> f t)) -> Traversal1 s t a b
 traversal1 = wander1
+
+traversed1 :: Traversable1 t => Traversal1 (t a) (t b) a b
+traversed1 = traversal1 traverse1
