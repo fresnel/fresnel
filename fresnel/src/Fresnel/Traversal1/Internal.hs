6faeac44af6d7986ed9f09710769371efff7ebaf
Rob Rix <rob.rix@me.com>
2024-09-21 20:23:22 -0400

    IsTraversal1.

--- fresnel/src/Fresnel/Traversal1/Internal.hs
+++ fresnel/src/Fresnel/Traversal1/Internal.hs
@@ -1,2 +1,8 @@
 module Fresnel.Traversal1.Internal
-() where
+( IsTraversal1
+) where
+
+import Fresnel.Lens.Internal (IsLens)
+import Fresnel.Profunctor.Traversing1 (Traversing1)
+
+class (IsLens p, Traversing1 p) => IsTraversal1 p
