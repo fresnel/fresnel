533b3c62e0ea0e64e55566f67b4b713032200e91
Rob Rix <rob.rix@me.com>
2024-09-21 21:04:22 -0400

    Heading.

--- fresnel/src/Fresnel/Fold1.hs
+++ fresnel/src/Fresnel/Fold1.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE RankNTypes #-}
 module Fresnel.Fold1
-( Fold1
+( -- * Relevant folds
+  Fold1
 ) where
 
 import Fresnel.Fold1.Internal (IsFold1)
 import Fresnel.Optic (Optic')
+
+-- Relevant folds
 
 type Fold1 s a = forall p . IsFold1 p => Optic' p s a
