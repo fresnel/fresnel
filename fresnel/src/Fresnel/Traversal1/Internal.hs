2c993df65bdef2d103ce9cf29368abd8925ecf68
Rob Rix <rob.rix@me.com>
2024-09-21 20:24:44 -0400

    Star IsTraversal1.

--- fresnel/src/Fresnel/Traversal1/Internal.hs
+++ fresnel/src/Fresnel/Traversal1/Internal.hs
@@ -2,10 +2,12 @@ module Fresnel.Traversal1.Internal
 ( IsTraversal1
 ) where
 
-import Data.Profunctor (Forget)
+import Data.Functor.Apply
+import Data.Profunctor (Forget, Star)
 import Fresnel.Lens.Internal (IsLens)
 import Fresnel.Profunctor.Traversing1 (Traversing1)
 
 class (IsLens p, Traversing1 p) => IsTraversal1 p
 
 instance Semigroup r => IsTraversal1 (Forget r)
+instance Apply f => IsTraversal1 (Star f)
