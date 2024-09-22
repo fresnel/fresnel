0f2c938d361acf58a6841c77a028fa3cb6d4071f
Rob Rix <rob.rix@me.com>
2024-09-22 15:28:07 -0400

    Traversing1.

--- fresnel/src/Fresnel/Profunctor/Star1.hs
+++ fresnel/src/Fresnel/Profunctor/Star1.hs
@@ -4,7 +4,9 @@ module Fresnel.Profunctor.Star1
   Star1(..)
 ) where
 
+import Data.Functor.Apply
 import Data.Profunctor
+import Fresnel.Profunctor.Traversing1
 
 -- | Just like 'Data.Profunctor.Star', but with some instances defined in terms of 'Data.Functor.Apply' instead of 'Applicative'. Used by 'Fresnel.Traversal1.Traversal1' & 'Fresnel.Fold1.Fold1'.
 newtype Star1 f a b = Star1 { runStar1 :: a -> f b }
@@ -15,3 +17,6 @@ instance Functor f => Profunctor (Star1 f) where
 instance Functor f => Strong (Star1 f) where
   first'  (Star1 h) = Star1 (\ (a, c) -> (,c) <$> h a)
   second' (Star1 h) = Star1 (\ (c, a) -> (c,) <$> h a)
+
+instance Apply f => Traversing1 (Star1 f) where
+  wander1 f (Star1 h) = Star1 (f h)
