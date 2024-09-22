372cb803aedac8dda04ca3f98e48d8ccc3c63962
Rob Rix <rob.rix@me.com>
2024-09-21 20:57:31 -0400

    Semigroup.

--- fresnel/src/Fresnel/Functor/Ap1.hs
+++ fresnel/src/Fresnel/Functor/Ap1.hs
@@ -7,3 +7,6 @@ import Data.Functor.Apply
 
 newtype Ap1 f a = Ap1 { getAp1 :: f a }
   deriving (Applicative, Apply, Functor, Monad)
+
+instance (Apply f, Semigroup a) => Semigroup (Ap1 f a) where
+  Ap1 a <> Ap1 b = Ap1 (liftF2 (<>) a b)
