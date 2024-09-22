f755567fc5f2d3bbba2f9650fb96d371eedeab1b
Rob Rix <rob.rix@me.com>
2024-09-22 15:33:36 -0400

    Star1 is Bicontravariant.

--- fresnel/src/Fresnel/Profunctor/Star1.hs
+++ fresnel/src/Fresnel/Profunctor/Star1.hs
@@ -5,7 +5,9 @@ module Fresnel.Profunctor.Star1
 ) where
 
 import Data.Functor.Apply
+import Data.Functor.Contravariant (Contravariant(..))
 import Data.Profunctor
+import Fresnel.Bifunctor.Contravariant (Bicontravariant(..))
 import Fresnel.Profunctor.Traversing1
 
 -- | Just like 'Data.Profunctor.Star', but with some instances defined in terms of 'Data.Functor.Apply' instead of 'Applicative'. Used by 'Fresnel.Traversal1.Traversal1' & 'Fresnel.Fold1.Fold1'.
@@ -20,3 +22,6 @@ instance Functor f => Strong (Star1 f) where
 
 instance Apply f => Traversing1 (Star1 f) where
   wander1 f (Star1 h) = Star1 (f h)
+
+instance Contravariant f => Bicontravariant (Star1 f) where
+  contrabimap f g (Star1 h) = Star1 (contramap g . h . f)
