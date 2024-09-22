7f13a5c9c150b29f1eea7d5424ea120d0cc90304
Rob Rix <rob.rix@me.com>
2024-09-22 13:53:10 -0400

    Show.

--- fresnel/src/Fresnel/Semigroup/Cons1.hs
+++ fresnel/src/Fresnel/Semigroup/Cons1.hs
@@ -4,9 +4,14 @@ module Fresnel.Semigroup.Cons1
   Cons1(..)
 ) where
 
+import Data.Foldable (toList)
+
 -- Non-empty cons lists
 
 newtype Cons1 a = Cons1 { runCons1 :: forall r . (a -> r) -> (a -> r -> r) -> r }
+
+instance Show a => Show (Cons1 a) where
+  showsPrec _ = showList . toList
 
 instance Foldable Cons1 where
   foldMap f (Cons1 r) = r f ((<>) . f)
