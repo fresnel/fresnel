7a6633ae492a9e9aacbcf7b37e6e610058659c28
Rob Rix <rob.rix@me.com>
2024-09-22 15:26:39 -0400

    Strong.

--- fresnel/src/Fresnel/Profunctor/Star1.hs
+++ fresnel/src/Fresnel/Profunctor/Star1.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 module Fresnel.Profunctor.Star1
 ( -- * Star1 profunctor
   Star1(..)
@@ -10,3 +11,7 @@ newtype Star1 f a b = Star1 { runStar1 :: a -> f b }
 
 instance Functor f => Profunctor (Star1 f) where
   dimap f g (Star1 h) = Star1 (fmap g . h . f)
+
+instance Functor f => Strong (Star1 f) where
+  first'  (Star1 h) = Star1 (\ (a, c) -> (,c) <$> h a)
+  second' (Star1 h) = Star1 (\ (c, a) -> (c,) <$> h a)
