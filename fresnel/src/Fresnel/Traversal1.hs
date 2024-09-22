c2109bade99b1467f6204225434b03f2dc3aaf31
Rob Rix <rob.rix@me.com>
2024-09-21 20:29:15 -0400

    traversal1.

--- fresnel/src/Fresnel/Traversal1.hs
+++ fresnel/src/Fresnel/Traversal1.hs
@@ -4,9 +4,13 @@ module Fresnel.Traversal1
   Traversal1
 , Traversal1'
 , IsTraversal1
+  -- * Construction
+, traversal1
 ) where
 
+import Data.Functor.Apply
 import Fresnel.Optic
+import Fresnel.Profunctor.Traversing1
 import Fresnel.Traversal1.Internal
 
 -- Relevant traversals
@@ -14,3 +18,9 @@ import Fresnel.Traversal1.Internal
 type Traversal1 s t a b = forall p . IsTraversal1 p => Optic p s t a b
 
 type Traversal1' s a = Traversal1 s s a a
+
+
+-- Construction
+
+traversal1 :: (forall f . Apply f => (a -> f b) -> (s -> f t)) -> Traversal1 s t a b
+traversal1 = wander1
