1cd2f336b2776698e9c3f9ff2675df609c6c7d68
Rob Rix <rob.rix@me.com>
2024-09-22 13:49:18 -0400

    Cons1.

--- fresnel/src/Fresnel/Semigroup/Cons1.hs
+++ fresnel/src/Fresnel/Semigroup/Cons1.hs
@@ -1,2 +1,9 @@
+{-# LANGUAGE RankNTypes #-}
 module Fresnel.Semigroup.Cons1
-() where
+( -- * Non-empty cons lists
+  Cons1(..)
+) where
+
+-- Non-empty cons lists
+
+newtype Cons1 a = Cons1 { runCons1 :: forall r . (a -> r) -> (a -> r -> r) -> r }
