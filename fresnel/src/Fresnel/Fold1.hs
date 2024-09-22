c0aad3d06355924b0cf0d2a9fc8ef31c7aaa9e1a
Rob Rix <rob.rix@me.com>
2024-09-21 21:12:17 -0400

    foldMap1ing.

--- fresnel/src/Fresnel/Fold1.hs
+++ fresnel/src/Fresnel/Fold1.hs
@@ -5,15 +5,18 @@ module Fresnel.Fold1
   -- * Construction
 , folded1
 , fold1ing
+, foldMap1ing
   -- * Elimination
 , foldMap1Of
 ) where
 
+import Data.Functor (void)
 import Data.Profunctor
 import Data.Profunctor.Unsafe ((#.), (.#))
 import Data.Semigroup.Foldable
 import Fresnel.Bifunctor.Contravariant
 import Fresnel.Fold1.Internal (IsFold1)
+import Fresnel.Functor.Ap1
 import Fresnel.Optic (Optic')
 import Fresnel.Traversal1
 
@@ -29,6 +32,10 @@ folded1 = rphantom . traversal1 traverse1_
 
 fold1ing :: Foldable1 t => (s -> t a) -> Fold1 s a
 fold1ing f = contrabimap f (const ()) . traversal1 traverse1_
+
+-- | Make a 'Fold1' by lifting a 'foldMap1'-like function.
+foldMap1ing :: (forall m . Semigroup m => (a -> m) -> (s -> m)) -> Fold1 s a
+foldMap1ing fm = rphantom . traversal1 (\ f -> getAp1 #. fm (Ap1 #. void . f))
 
 
 -- Elimination
