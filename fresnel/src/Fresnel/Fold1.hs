7b00f402d0762da5e64925cecc1674accd2976dd
Rob Rix <rob.rix@me.com>
2024-09-21 21:07:01 -0400

    folded1.

--- fresnel/src/Fresnel/Fold1.hs
+++ fresnel/src/Fresnel/Fold1.hs
@@ -2,11 +2,22 @@
 module Fresnel.Fold1
 ( -- * Relevant folds
   Fold1
+  -- * Construction
+, folded1
 ) where
 
+import Data.Semigroup.Foldable
+import Fresnel.Bifunctor.Contravariant
 import Fresnel.Fold1.Internal (IsFold1)
 import Fresnel.Optic (Optic')
+import Fresnel.Profunctor.Traversing1
 
 -- Relevant folds
 
 type Fold1 s a = forall p . IsFold1 p => Optic' p s a
+
+
+-- Construction
+
+folded1 :: Foldable1 t => Fold1 (t a) a
+folded1 = rphantom . wander1 traverse1_
